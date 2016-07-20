b38278dcc711494beb37fa6b6c383d68a4aafebf
Patrick Williams <patrick@stwcx.xyz>
2016-07-20 12:36:56 -0500

    Add tuple_to_array template
    C++ template to convert tuple<T...> to array<T, N>.
    
    Change-Id: Ia9dff382f9c57d7608edaec281cf7d867aeed4b2
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ sdbusplus/utility/tuple_to_array.hpp
@@ -0,0 +1,59 @@
+#pragma once
+
+#include <utility>
+#include <tuple>
+#include <array>
+
+namespace sdbusplus
+{
+
+namespace utility
+{
+
+namespace details
+{
+
+/** tuple_to_array - Create std::array from std::tuple.
+ *
+ *  @tparam V - Type of the first tuple element.
+ *  @tparam Types - Sequence of types for remaining elements, which must
+ *                  be automatically castable to V.
+ *  @tparam I - Sequence of integral indexes (0...N) for each tuple elemeent.
+ *
+ *  @param tuple - Tuple of N same-typed elements.
+ *  @param [unnamed] - std::integer_sequence of tuple's index values.
+ *
+ *  @return A std::array where each I-th element is tuple's I-th element.
+ */
+template <typename V, typename... Types,
+          std::size_t... I>
+constexpr auto tuple_to_array(
+    std::tuple<V, Types...>&& tuple,
+    std::integer_sequence<std::size_t, I...>)
+{
+    return std::array<const V, std::tuple_size<std::tuple<V, Types...>>::value>
+    ({ std::get<I>(tuple)..., });
+}
+
+} // namespace details
+
+/** tuple_to_array - Create std::array from std::tuple.
+ *
+ *  @tparam Types - Sequence of types of the tuple elements.
+ *  @tparam I - std::integer_sequence of indexes (0..N) for each tuple element.
+ *
+ *  @param tuple - Tuple of N same-typed elements.
+ *
+ *  @return A std::array where each I-th element is tuple's I-th element.
+ */
+template <typename... Types,
+          typename I = std::make_index_sequence<
+              std::tuple_size<std::tuple<Types...>>::value>>
+constexpr auto tuple_to_array(std::tuple<Types...>&& tuple)
+{
+    return details::tuple_to_array(std::move(tuple), I());
+}
+
+} // namespace utility
+
+} // namespace sdbusplus

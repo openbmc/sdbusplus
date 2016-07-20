8cd93665c1daf3fe75589aefa708bdaa369a3954
Patrick Williams <patrick@stwcx.xyz>
2016-07-20 12:36:56 -0500

    Add tuple_to_array template
    C++ template to convert tuple<T...> to array<T, N>.
    
    Change-Id: Ia9dff382f9c57d7608edaec281cf7d867aeed4b2
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ test/utility/tuple_to_array.cpp
@@ -0,0 +1,17 @@
+#include <sdbusplus/utility/tuple_to_array.hpp>
+#include <cassert>
+
+int main()
+{
+    std::array<char, 3> a{'a', 'b', 'c'};
+    auto t = std::make_tuple('a', 'b', 'c');
+
+    assert(a == sdbusplus::utility::tuple_to_array(std::move(t)));
+
+    std::array<int, 4> b{1, 2, 3, 4};
+    auto t2 = std::make_tuple(1, 2, 3, 4);
+
+    assert(b == sdbusplus::utility::tuple_to_array(std::move(t2)));
+
+    return 0;
+}

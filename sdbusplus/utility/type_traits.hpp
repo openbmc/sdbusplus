0c841a17f53aca94cc4f66943c86169def9e5186
Patrick Williams <patrick@stwcx.xyz>
2016-07-20 12:34:24 -0500

    Add array_to_ptr template
    C++ template to convert T[] type to T* type.
    
    Change-Id: I91c63b5d14b77f82d3deea3772990912dfcd4277
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ sdbusplus/utility/type_traits.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <type_traits>
+
+namespace sdbusplus
+{
+
+namespace utility
+{
+
+/** @brief Convert T[N] to T* if is_same<Tbase,T>
+ *
+ *  @tparam Tbase - The base type expected.
+ *  @tparam T - The type to convert.
+ */
+template <typename Tbase, typename T>
+using array_to_ptr_t = typename std::conditional_t<
+        std::is_array<T>::value,
+        std::conditional_t<std::is_same<Tbase,
+                                        std::remove_extent_t<T>>::value,
+                           std::add_pointer_t<std::remove_extent_t<T>>,
+                           T>,
+        T>;
+
+} // namespace utility
+
+} // namespace sdbusplus

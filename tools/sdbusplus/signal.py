28053214a71dc9a2373597a33dc63a71a6636793
Patrick Williams <patrick@stwcx.xyz>
2016-10-09 16:23:20 -0500

    signal: render as markdown
    Change-Id: Ibd5da3a0558b6ddbad3d0b64ee692746dc2ced32
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- tools/sdbusplus/signal.py
+++ tools/sdbusplus/signal.py
@@ -10,3 +10,8 @@ class Signal(NamedElement):
         properties = values.get('properties', [])
         for p in properties:
             self.properties.append(Property(p))
+
+    def markdown(self, loader):
+        template = loader.get_template("signal.mako.md")
+        return template.render(signal=self, loader=loader)
+

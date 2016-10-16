2dae38fe8fe98d20a5dd9737c1c96a32cb31cc22
Patrick Williams <patrick@stwcx.xyz>
2016-10-16 18:09:00 -0500

    sdbus++: generate callback methods for server bindings
    Create sd-bus message handler methods as callbacks for object
    method calls.  These generated callbacks will extract the
    parameters from the message, call the virtual-method implementation,
    and pack the response into a method-return message, which is sent
    back to the caller.
    
    Change-Id: Ia3872bbbbfdb57375a1ffdde7e64296271cdd532
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- tools/templates/method.mako.prototype.hpp
+++ tools/templates/method.mako.prototype.hpp
@@ -13,17 +13,30 @@
         return ",\n            ".\
             join([ parameter(p, defaultValue) for p in method.parameters ])
 
+    def parameters_as_local():
+        return "{};\n    ".join([ parameter(p) for p in method.parameters ])
+
+    def parameters_as_list():
+        return ", ".join([ p.camelCase for p in method.parameters ])
+
     def parameter(p, defaultValue=False):
         r = "%s %s" % (p.typeName, p.camelCase)
         if defaultValue:
             r += default_value(p)
         return r
 
+    def returns_as_tuple_index(tuple):
+        return ", ".join([ "std::move(std::get<%d>(%s))" % (i,tuple) \
+                for i in range(len(method.returns))])
+
     def default_value(p):
         if p.defaultValue != None:
             return " = " + str(p.defaultValue)
         else:
             return ""
+
+    def interface_name():
+        return interface.name.split('.').pop()
 %>
 ###
 ### Emit 'header'
@@ -54,4 +67,39 @@
          */
         static int _callback_${ method.CamelCase }(
             sd_bus_message*, void*, sd_bus_error*);
+###
+### Emit 'callback-cpp'
+###
+    % elif ptype == 'callback-cpp':
+int ${interface_name()}::_callback_${ method.CamelCase }(
+        sd_bus_message* msg, void* context, sd_bus_error* error)
+{
+    ### Need to add a ref to msg since we attached it to an sdbusplus::message.
+    auto m = sdbusplus::message::message(sd_bus_message_ref(msg));
+
+    % if len(method.parameters) != 0:
+    ${parameters_as_local()}{};
+
+    m.read(${parameters_as_list()});
+    % endif
+
+    auto o = static_cast<${interface_name()}*>(context);
+    % if len(method.returns) != 0:
+    auto r = \
+    %endif
+    o->${ method.camelCase }(${parameters_as_list()});
+
+    auto reply = m.new_method_return();
+    % if len(method.returns) == 0:
+    // No data to append on reply.
+    % elif len(method.returns) == 1:
+    reply.append(std::move(r));
+    % else:
+    reply.append(${returns_as_tuple_index("r")});
+    % endif
+
+    sdbusplus::bus::method_return(reply);
+
+    return 0;
+}
     % endif

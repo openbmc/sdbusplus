7b584554587dc69a713d8eb1211c7da89f928b93
Adriana Kobylak <anoo@us.ibm.com>
2016-12-06 14:30:23 -0600

    Store transaction id in thread local storage
    Interfaces to set/get the transaction id saved in thread
    local storage.
    
    Change-Id: Id1df415d9cc05e4b96817c7a05c1be7052d05c65
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- sdbusplus/server/transaction.hpp
+++ sdbusplus/server/transaction.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <thread>
 #include <sdbusplus/bus.hpp>
 
 namespace sdbusplus
@@ -11,6 +12,8 @@ namespace transaction
 {
 namespace details
 {
+    // Thread local storage
+    thread_local uint64_t transactionId = 0;
 
     /** @brief Generate transaction id from hashigh the  bus name
       * and message cookie.
@@ -23,14 +26,16 @@ namespace details
                      sdbusplus::message::message& msg)
     {
         const char* uniqueName = bus.get_unique_name();
+        uint64_t cookie = msg.get_cookie();
+
+        // TODO Handle errors from sdbusplus once they're available. If bus
+        // or msg return an error, generate a random id.
 
         // Convert const char* to std::string since std::hash does not
         // have support for char
         std::string uniqueNameStr(uniqueName, 0, strlen(uniqueName));
 
         size_t hashName = std::hash<std::string>{}(uniqueNameStr);
-
-        uint64_t cookie = msg.get_cookie();
         size_t hashCookie = std::hash<uint64_t>{}(cookie);
 
         // Combine the hash for the unique name and cookie following the
@@ -40,7 +45,63 @@ namespace details
         return hashName;
     }
 
+    /** @brief Generate a random transaction id.
+      *
+      * @return The generated transaction id.
+      */
+    auto generate_random_id()
+    {
+        // Arbitrarily use the current time and thread id to generate a
+        // transaction id hash.
+
+        auto hash1 = std::hash<int>{}(std::time(nullptr));
+        auto hash2 = std::hash<std::thread::id>{}(std::this_thread::get_id());
+
+        // Combine the hashes by using the algorithm from boost::hash_combine()
+        hash1^= hash2 + 0x9e3779b9 + (hash1 << 6) + (hash1 >> 2);
+
+        return hash1;
+    }
+
+    /** @brief Set transaction id
+      *
+      * @param[in] value - Desired value for the transaction id
+      */
+    void set_id(uint64_t value)
+    {
+        transactionId = value;
+    }
+
 } // details
+
+    /** @brief Get transaction id.
+      * If it is not initialized, generate a random one.
+      *
+      * @return The value of the transaction id
+      */
+    uint64_t get_transaction_id()
+    {
+        if (!details::transactionId)
+        {
+            auto random = details::generate_random_id();
+            details::set_id(random);
+        }
+
+        return details::transactionId;
+    }
+
+    /** @brief Generate and save the transaction id info
+      *
+      * @param[in] bus - Dbus bus
+      * @param[in] msg - Dbus message
+      */
+    void enter(sdbusplus::bus::bus& bus,
+               sdbusplus::message::message& msg)
+    {
+        auto value = details::generate_id(bus, msg);
+        details::set_id(value);
+    }
+
 } // transaction
 } // server
 } // sdbusplus

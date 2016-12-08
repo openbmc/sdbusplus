6ffb8f09034ec9f840d0a88dfaef3557398c2e0e
Adriana Kobylak <anoo@us.ibm.com>
2016-12-08 13:01:18 -0600

    Initialize transaction id
    Create interface to initialize the transaction id value by
    using the sdbus bus and message information.
    Generate a random value if the transaction id has not been
    initialized and a caller requests it. This is the scenario for
    clients that have not (or will not) make sdbus calls but still
    log entries into the journal.
    
    Change-Id: Ia7b926fd3f83963fac01fbd1ce4cdd59d2224008
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- sdbusplus/server/transaction.hpp
+++ sdbusplus/server/transaction.hpp
@@ -43,8 +43,26 @@ namespace details
         return hashName;
     }
 
+    /** @brief Generate a random transaction id.
+      *
+      * @return The generated transaction id.
+      */
+    auto generate_random_id()
+    {
+        // Arbitrarily use the current time and thread id to generate a
+        // transaction id hash.
+
+        auto hash1 = std::hash<int>{}(std::time(nullptr));
+        auto hash2 = std::hash<std::thread::id>{}(std::this_thread::get_id());
+
+        // Combine the hashes by using the algorithm from boost::hash_combine()
+        hash1^= hash2 + 0x9e3779b9 + (hash1 << 6) + (hash1 >> 2);
+
+        return hash1;
+    }
+
     /** @brief Set transaction id
-      * 
+      *
       * @param[in] value - Desired value for the transaction id
       */
     void set_id(uint64_t value)
@@ -54,13 +72,31 @@ namespace details
 
 } // details
 
-    /** @brief Get transaction id
-      * 
+    /** @brief Get transaction id. If it is not initialized, generate a random one.
+      *
       * @return The value of the transaction id
       */
     uint64_t get_id()
     {
+        if (!details::transactionId)
+        {
+            auto random = details::generate_random_id();
+            details::set_id(random);
+        }
+
         return details::transactionId;
+    }
+
+    /** @brief Generate and save the transaction id info
+      *
+      * @param[in] bus - Dbus bus
+      * @param[in] msg - Dbus message
+      */
+    void enter(sdbusplus::bus::bus& bus,
+               sdbusplus::message::message& msg)
+    {
+        auto value = details::generate_id(bus, msg);
+        details::set_id(value);
     }
 
 } // transaction

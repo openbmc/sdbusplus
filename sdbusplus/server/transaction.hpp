add2424bd99c5fdc7abfb5dffe9bf4a0ecb20680
Adriana Kobylak <anoo@us.ibm.com>
2017-01-04 12:42:52 -0600

    Generate transaction id
    Use the bus unique name and message cookie to generate a
    unique hash called transaction id.
    The generated transaction id will be saved off in the server
    thread local storage. The function is in the details namespace
    as it's just the generation algorithm. Subsequent commits
    will add public functions to interact with the id.
    
    Change-Id: Ida52774857a7ed2cca9364da15f84a30d2c3af06
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

---
+++ sdbusplus/server/transaction.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <sdbusplus/bus.hpp>
+
+namespace sdbusplus
+{
+namespace server
+{
+namespace transaction
+{
+namespace details
+{
+
+    /** @brief Generate a transaction id from hashing the bus name
+      * and message cookie.
+      *
+      * @param[in] bus - Dbus bus
+      * @param[in] msg - Dbus message
+      * @return The generated transaction id.
+      */
+    auto generate_id(sdbusplus::bus::bus& bus,
+                     sdbusplus::message::message& msg)
+    {
+        auto uniqueName = bus.get_unique_name();
+        auto cookie = msg.get_cookie();
+
+        auto hashName = std::hash<std::string>{}(uniqueName);
+        auto hashCookie = std::hash<uint64_t>{}(cookie);
+
+        // Combine the hash for the unique name and cookie following the
+        // algorithm from boost::hash_combine() (Documented in the boost lib)
+        hashName^= hashCookie + 0x9e3779b9 + (hashName << 6) + (hashName >> 2);
+
+        return hashName;
+    }
+
+} // details
+} // transaction
+} // server
+} // sdbusplus

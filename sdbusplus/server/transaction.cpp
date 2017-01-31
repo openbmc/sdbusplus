1afb4e60b8f4754d202af4f16822cea1c5c22eb3
Adriana Kobylak <anoo@us.ibm.com>
2017-01-31 11:10:01 -0600

    Store transaction id in thread local storage
    Implement the transaction id variable in a .C file and
    create an sdbusplus library with it.
    Create interfaces to set/get the transaction id.
    
    Change-Id: Id1df415d9cc05e4b96817c7a05c1be7052d05c65
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

---
+++ sdbusplus/server/transaction.cpp
@@ -0,0 +1,18 @@
+#include "transaction.hpp"
+
+namespace sdbusplus
+{
+namespace server
+{
+namespace transaction
+{
+namespace details
+{
+
+// Transaction Id
+thread_local uint64_t id = 0;
+
+} // namespace details
+} // namespace transaction
+} // namespace server
+} // namespace sdbusplus

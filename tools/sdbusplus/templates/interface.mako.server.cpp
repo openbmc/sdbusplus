8a685d3fd4a892c7367d3e84d8b91074ab6eafec
Patrick Williams <patrick@stwcx.xyz>
2017-02-02 09:24:56 -0600

    sdbus++: Add server method for get-property
    Add a method to the server bindings that gets properties
    by looking up the name from a string.
    
    Change-Id: Ie8904690f13f33f3399e38ac6b7c1cfac7796a3f
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- tools/sdbusplus/templates/interface.mako.server.cpp
+++ tools/sdbusplus/templates/interface.mako.server.cpp
@@ -157,6 +157,17 @@ val);
     % endfor
 }
 
+auto ${classname}::getPropertyByName(const std::string& name) ->
+        PropertiesVariant
+{
+    % for p in interface.properties:
+    if (name == "${p.name}")
+    {
+        return ${p.camelCase}();
+    }
+    % endfor
+}
+
     % for e in interface.enums:
 
 namespace

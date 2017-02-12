4a230ba6633ecf52c7a6b3688a78f2976a913945
Adriana Kobylak <anoo@linux.vnet.ibm.com>
2017-02-12 09:12:37 -0600

    Disable transaction id support by default
    Allow the transaction id support to be enabled via
    configure options.
    In the code, ifdef the code based if transaction support
    is desired.
    
    Change-Id: I98695268397f437b6fe14621f820fbcaecb45b7a
    Signed-off-by: Adriana Kobylak <anoo@us.ibm.com>

--- sdbusplus/server.hpp
+++ sdbusplus/server.hpp
@@ -1,11 +1,1 @@
-#pragma once
-
-#include <sdbusplus/bus.hpp>
-#include <sdbusplus/vtable.hpp>
-
-#include <sdbusplus/server/interface.hpp>
-#include <sdbusplus/server/manager.hpp>
-#include <sdbusplus/server/object.hpp>
-#include <sdbusplus/server/match.hpp>
-#include <sdbusplus/server/bindings.hpp>
-#include <sdbusplus/server/transaction.hpp>
+# Empty file so that 'configure' attempts to generate server.hpp

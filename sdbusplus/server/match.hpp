a24faff62f160cfd168d8695c0702c0604c6e069
Brad Bishop <bradleyb@fuzziesquirrel.com>
2017-02-28 22:01:40 -0500

    match: Add support for arbitrary callback types
    Add a constructor that accepts lambdas and other callable
    types with a signature of void(message&) for callbacks.
    
    This enables match callbacks like the following:
    
    auto bus = sdbusplus::bus::get_default();
    auto context = 100ull;
    match(bus, "type=signal", [context](auto& msg){ std::cout << context << "\n";});
    
    Change-Id: I8302fb51eac807b989f5bccf52e843bb38938476
    Signed-off-by: Brad Bishop <bradleyb@fuzziesquirrel.com>

--- sdbusplus/server/match.hpp
+++ sdbusplus/server/match.hpp
@@ -20,13 +20,13 @@ struct match_impl
     match_impl() = delete;
     match_impl(const match_impl&) = delete;
     match_impl& operator=(const match_impl&) = delete;
-    match_impl(match_impl&&) = default;
-    match_impl& operator=(match_impl&&) = default;
+    match_impl(match_impl&&) = delete;
+    match_impl& operator=(match_impl&&) = delete;
     ~match_impl() = default;
 
     match_impl(sdbusplus::bus::bus& bus, const char* match,
           sd_bus_message_handler_t handler, void* context = nullptr)
-                : _slot(nullptr)
+                : _slot(nullptr), _callback(nullptr)
     {
         sd_bus_slot* slot = nullptr;
         sd_bus_add_match(bus.get(), &slot, match, handler, context);
@@ -34,8 +34,54 @@ struct match_impl
         _slot = decltype(_slot){slot};
     }
 
+    template <class Callable>
+    explicit match_impl(sdbusplus::bus::bus& bus,
+                        const char* match,
+                        Callable&& cb)
+                : _slot(nullptr),
+                _callback(std::make_unique<callback<Callable>>(
+                            std::forward<Callable>(cb)))
+    {
+        sd_bus_slot* slot = nullptr;
+        sd_bus_add_match(
+                bus.get(), &slot, match, match_impl::sd_callback, this);
+
+        _slot = decltype(_slot){slot};
+    }
+
     private:
+        struct callback_base
+        {
+            virtual ~callback_base() = default;
+            virtual void operator()(message::message& m) = 0;
+        };
+
+        template <class Callable>
+        struct callback final : public callback_base
+        {
+            explicit callback(Callable&& cb)
+                : _cb(std::forward<Callable>(cb)) {}
+
+            void operator()(message::message& m)
+            {
+                _cb(m);
+            }
+
+            Callable _cb;
+        };
+
+        static auto sd_callback(
+                sd_bus_message* m, void* context, sd_bus_error* e)
+        {
+            auto me = static_cast<match_impl *>(context);
+            auto& callback = *me->_callback;
+            message::message msg(m);
+            callback(msg);
+            return 0;
+        }
+
         slot::slot _slot;
+        std::unique_ptr<callback_base> _callback;
 };
 
 } // namespace details
@@ -68,6 +114,22 @@ struct match
           sd_bus_message_handler_t handler, void* context = nullptr)
                 : _impl(std::make_unique<details::match_impl>(
                             bus, match, handler, context))
+    {
+    }
+
+    /** @brief Register a signal match.
+     *
+     *  @param[in] bus - The bus to register on.
+     *  @param[in] match - The match to register.
+     *  @param[in] callback - The callback for matches.
+     */
+    template <class Callable>
+    explicit match(
+            sdbusplus::bus::bus& bus,
+            const char* match,
+            Callable&& cb) :
+        _impl(std::make_unique<details::match_impl>(
+                    bus, match, std::forward<Callable>(cb)))
     {
     }
 

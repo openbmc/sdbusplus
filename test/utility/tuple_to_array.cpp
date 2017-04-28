fe2d5ad787a504b1c5d9f77b6584c4a5b02215fc
Patrick Williams <patrick@stwcx.xyz>
2017-04-28 16:00:19 -0500

    test: convert tuple_to_array to gtest
    Change-Id: Ie110fba8f1961ba0e1a46c670d98a2351a46d061
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

--- test/utility/tuple_to_array.cpp
+++ test/utility/tuple_to_array.cpp
@@ -1,17 +1,18 @@
 #include <sdbusplus/utility/tuple_to_array.hpp>
-#include <cassert>
+#include <gtest/gtest.h>
 
-int main()
+TEST(TupleToArray, Test3Chars)
 {
     std::array<char, 3> a{'a', 'b', 'c'};
     auto t = std::make_tuple('a', 'b', 'c');
 
-    assert(a == sdbusplus::utility::tuple_to_array(std::move(t)));
+    ASSERT_EQ(a, sdbusplus::utility::tuple_to_array(std::move(t)));
+}
 
+TEST(TupleToArray, Test4Ints)
+{
     std::array<int, 4> b{1, 2, 3, 4};
     auto t2 = std::make_tuple(1, 2, 3, 4);
 
-    assert(b == sdbusplus::utility::tuple_to_array(std::move(t2)));
-
-    return 0;
+    ASSERT_EQ(b, sdbusplus::utility::tuple_to_array(std::move(t2)));
 }

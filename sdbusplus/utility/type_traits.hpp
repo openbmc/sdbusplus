b6650a8ab0c98cc5886bc51993b0e65b408ac4a1
James Feist <james.feist@linux.intel.com>
2018-04-05 15:28:16 -0700

    Add boost asio async connection
    Add an async connection and example of usage. This
    connection inherits the bus object and allows async
    method calls using boost asio. Most of these concepts
    are from boost-dbus.
    
    Change-Id: I33b5349d543c9ff4b6ee1ce15346c709c052e1ae
    Tested: Compiled and ran asio-example on bmc.
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- sdbusplus/utility/type_traits.hpp
+++ sdbusplus/utility/type_traits.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <type_traits>
+#include <tuple>
 
 namespace sdbusplus
 {
@@ -19,6 +20,28 @@ using array_to_ptr_t = typename std::conditional_t<
     std::conditional_t<std::is_same<Tbase, std::remove_extent_t<T>>::value,
                        std::add_pointer_t<std::remove_extent_t<T>>, T>,
     T>;
+
+// Small helper class for stripping off the error code from the function
+// argument definitions so unpack can be called appropriately
+template <typename T> struct strip_first_arg
+{
+};
+
+template <typename FirstArg, typename... Rest>
+struct strip_first_arg<std::tuple<FirstArg, Rest...>>
+{
+    using type = std::tuple<Rest...>;
+};
+
+// helper class to remove const and reference from types
+template <typename T> struct decay_tuple
+{
+};
+
+template <typename... Args> struct decay_tuple<std::tuple<Args...>>
+{
+    using type = std::tuple<typename std::decay<Args>::type...>;
+};
 
 } // namespace utility
 

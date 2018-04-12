9c4774241421bfbc1435bddc2ab5465590bf46af
Patrick Venture <venture@google.com>
2018-04-12 14:14:42 -0700

    sdbusplus::message::message Add injection w/ sdbus
    This adds an sdbus interface and an implementation and
    mock and integrates that with sdbusplus::message::message.
    
    Because sdbusplus::message::message is really a data interface, it was
    more appropriate to implement the interface at the sd-bus level.  Also,
    you can't mock the template methods.
    
    Tested: Verified this builds with the SDK, and the quanta-q71l image
    continues to work.  Verified the mock works as expected with some
    example unit-tests.
    Note: This was originally part of another patchset.
    Change-Id: I133770fdeec327e018b6c587a7992cbccc7b3f10
    Signed-off-by: Patrick Venture <venture@google.com>

---
+++ sdbusplus/test/sdbus_mock.hpp
@@ -0,0 +1,56 @@
+#pragma once
+
+#include "gmock/gmock.h"
+
+#include <sdbusplus/sdbus.hpp>
+
+namespace sdbusplus
+{
+
+class SdBusMock : public SdBusInterface
+{
+  public:
+    virtual ~SdBusMock(){};
+
+    MOCK_METHOD1(sd_bus_message_ref, sd_bus_message *(sd_bus_message *));
+    MOCK_METHOD3(sd_bus_message_append_basic,
+                 int(sd_bus_message *, char, const void *));
+    MOCK_METHOD2(sd_bus_message_at_end, int(sd_bus_message *, int));
+    MOCK_METHOD1(sd_bus_message_close_container, int(sd_bus_message *));
+    MOCK_METHOD3(sd_bus_message_enter_container,
+                 int(sd_bus_message *, char, const char *));
+    MOCK_METHOD1(sd_bus_message_exit_container, int(sd_bus_message *));
+
+    MOCK_METHOD1(sd_bus_message_get_bus, sd_bus *(sd_bus_message *));
+    MOCK_METHOD2(sd_bus_message_get_cookie, int(sd_bus_message *, uint64_t *));
+    MOCK_METHOD1(sd_bus_message_get_destination,
+                 const char *(sd_bus_message *));
+    MOCK_METHOD1(sd_bus_message_get_interface, const char *(sd_bus_message *));
+    MOCK_METHOD1(sd_bus_message_get_member, const char *(sd_bus_message *));
+    MOCK_METHOD1(sd_bus_message_get_path, const char *(sd_bus_message *));
+    MOCK_METHOD1(sd_bus_message_get_sender, const char *(sd_bus_message *));
+    MOCK_METHOD2(sd_bus_message_get_signature,
+                 const char *(sd_bus_message *, int));
+
+    MOCK_METHOD3(sd_bus_message_is_method_call,
+                 int(sd_bus_message *, const char *, const char *));
+    MOCK_METHOD2(sd_bus_message_is_method_error,
+                 int(sd_bus_message *, const char *));
+    MOCK_METHOD3(sd_bus_message_is_signal,
+                 int(sd_bus_message *, const char *, const char *));
+
+    MOCK_METHOD2(sd_bus_message_new_method_return,
+                 int(sd_bus_message *, sd_bus_message **));
+    MOCK_METHOD3(sd_bus_message_read_basic,
+                 int(sd_bus_message *, char, void *));
+
+    MOCK_METHOD2(sd_bus_message_skip, int(sd_bus_message *, const char *));
+    MOCK_METHOD3(sd_bus_message_verify_type,
+                 int(sd_bus_message *, char, const char *));
+    MOCK_METHOD3(sd_bus_send, int(sd_bus *, sd_bus_message *, uint64_t *));
+
+    MOCK_METHOD3(sd_bus_message_open_container,
+                 int(sd_bus_message *, char, const char *));
+};
+
+} // namespace sdbusplus

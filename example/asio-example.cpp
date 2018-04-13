5e629a92fee127d5b336f243410601c31f4364ed
James Feist <james.feist@linux.intel.com>
2018-04-13 15:43:13 -0700

    Add sbusplus properties server
    Add properties server similar to boost-dbus that allows
    for creating dbus interfaces during runtime. This adds
    support for creating methods and get / set properties.
    
    Tested: Ran asio-example on bmc, and updated fru-device.
    Change-Id: I19881049f4307fe9c68f78df8854f14afdd6c362
    Signed-off-by: James Feist <james.feist@linux.intel.com>

--- example/asio-example.cpp
+++ example/asio-example.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include <ctime>
+#include <chrono>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/server.hpp>
 #include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
 #include <boost/asio.hpp>
 
 int main()
@@ -12,8 +15,9 @@ int main()
     using message = sdbusplus::message::message;
     // setup connection to dbus
     boost::asio::io_service io;
-
     auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+
+    // test async method call and async send
     auto mesg =
         conn->new_method_call("xyz.openbmc_project.ObjectMapper",
                               "/xyz/openbmc_project/object_mapper",
@@ -23,7 +27,6 @@ int main()
     static const std::vector<std::string> interfaces = {
         "xyz.openbmc_project.Sensor.Value"};
     mesg.append("/xyz/openbmc_project/Sensors", depth, interfaces);
-
     conn->async_send(mesg, [](boost::system::error_code ec, message& ret) {
         std::cout << "async_send callback\n";
         if (ec || ret.is_method_error())
@@ -38,7 +41,6 @@ int main()
             std::cout << item.first << "\n";
         }
     });
-
     conn->async_method_call(
         [](boost::system::error_code ec, GetSubTreeType& subtree) {
             std::cout << "async_method_call callback\n";
@@ -58,6 +60,55 @@ int main()
         "xyz.openbmc_project.ObjectMapper", "GetSubTree",
         "/org/openbmc/control", 2, std::vector<std::string>());
 
+    // test object server
+    conn->request_name("xyz.openbmc_project.asio-test");
+    auto server = sdbusplus::asio::object_server(conn);
+    std::shared_ptr<sdbusplus::asio::dbus_interface> iface =
+        server.add_interface("/xyz/openbmc_project/test",
+                             "xyz.openbmc_project.test");
+    // test generic properties
+    iface->register_property("int", 33,
+                             sdbusplus::asio::PropertyPermission::readWrite);
+    std::vector<std::string> myStringVec = {"some", "test", "data"};
+    std::vector<std::string> myStringVec2 = {"more", "test", "data"};
+
+    iface->register_property("myStringVec", myStringVec,
+                             sdbusplus::asio::PropertyPermission::readWrite);
+    iface->register_property("myStringVec2", myStringVec2);
+
+    // test properties with specialized callbacks
+    iface->register_property("lessThan50", 23,
+                             // custom set
+                             [](const int& req, int& propertyValue) {
+                                 if (req >= 50)
+                                 {
+                                     return -EINVAL;
+                                 }
+                                 propertyValue = req;
+                                 return 1; // success
+                             });
+    iface->register_property(
+        "TrailTime", std::string("foo"),
+        // custom set
+        [](const std::string& req, std::string& propertyValue) {
+            propertyValue = req;
+            return 1; // success
+        },
+        // custom get
+        [](const std::string& property) {
+            auto now = std::chrono::system_clock::now();
+            auto timePoint = std::chrono::system_clock::to_time_t(now);
+            return property + std::ctime(&timePoint);
+
+        });
+
+    // test method creation
+    iface->register_method("TestMethod", [](const int32_t& callCount) {
+        return "success: " + std::to_string(callCount);
+    });
+
+    iface->initialize();
+    iface->set_property("int", 45);
     io.run();
 
     return 0;

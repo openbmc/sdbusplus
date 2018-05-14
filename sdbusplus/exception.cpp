20db3bf2f7aabea70aacfae341a8f43cf5e0dacc
William A. Kennington III <wak@google.com>
2018-05-14 16:18:37 -0700

    exception: Add an exception for holding sd_bus_errors
    This enables us to return more error information about the underlying
    sd_bus api call that failed. This will make error output more verbose
    and enable us to apply more granular filters to specific dbus related
    errors.
    
    Change-Id: I3811139011753d7bd596b46cb69da68f7826ed7b
    Signed-off-by: William A. Kennington III <wak@google.com>

--- sdbusplus/exception.cpp
+++ sdbusplus/exception.cpp
@@ -5,6 +5,83 @@ namespace sdbusplus
 namespace exception
 {
 
+SdBusError::SdBusError(int error, const char* prefix) :
+    std::system_error(error, std::generic_category()), error(SD_BUS_ERROR_NULL)
+{
+    if (error == ENOMEM ||
+        sd_bus_error_set_errno(&this->error, error) == -ENOMEM)
+    {
+        throw std::bad_alloc();
+    }
+
+    populateMessage(prefix);
+}
+
+SdBusError::SdBusError(sd_bus_error error, const char* prefix) :
+    std::system_error(sd_bus_error_get_errno(&error), std::generic_category()),
+    error(error)
+{
+    populateMessage(prefix);
+}
+
+SdBusError::SdBusError(SdBusError&& other)
+{
+    move(std::move(other));
+}
+
+SdBusError& SdBusError::operator=(SdBusError&& other)
+{
+    if (this != &other)
+    {
+        move(std::move(other));
+    }
+    return *this;
+}
+
+SdBusError::~SdBusError()
+{
+    sd_bus_error_free(&error);
+}
+
+const char* SdBusError::name() const noexcept
+{
+    return error.name;
+}
+
+const char* SdBusError::description() const noexcept
+{
+    return error.message;
+}
+
+const char* SdBusError::what() const noexcept
+{
+    return full_message.c_str();
+}
+
+void SdBusError::populateMessage(const char* prefix)
+{
+    full_message = prefix;
+    if (error.name)
+    {
+        full_message += ": ";
+        full_message += error.name;
+    }
+    if (error.message)
+    {
+        full_message += ": ";
+        full_message += error.message;
+    }
+}
+
+void SdBusError::move(SdBusError&& other)
+{
+    sd_bus_error_free(&error);
+    error = other.error;
+    other.error = SD_BUS_ERROR_NULL;
+
+    full_message = std::move(other.full_message);
+}
+
 const char* InvalidEnumString::name() const noexcept
 {
     return errName;

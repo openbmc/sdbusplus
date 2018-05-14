26998431c36acb7655b7593022b9a3f1ae147465
William A. Kennington III <wak@google.com>
2018-05-14 16:18:37 -0700

    exception: Add an exception for holding sd_bus_errors
    This enables us to return more error information about the underlying
    sd_bus api call that failed. This will make error output more verbose
    and enable us to apply more granular filters to specific dbus related
    errors.
    
    Change-Id: I3811139011753d7bd596b46cb69da68f7826ed7b
    Signed-off-by: William A. Kennington III <wak@google.com>

--- sdbusplus/exception.cpp
+++ sdbusplus/exception.cpp
@@ -5,6 +5,56 @@ namespace sdbusplus
 namespace exception
 {
 
+SdBusError::SdBusError(int error, const char *description)
+    : std::system_error(error, std::generic_category()),
+      error(SD_BUS_ERROR_NULL)
+{
+    if (error == ENOMEM ||
+            sd_bus_error_set_errno(&this->error, error) == -ENOMEM)
+    {
+        throw std::bad_alloc();
+    }
+
+    populateMessage(description);
+}
+
+SdBusError::SdBusError(sd_bus_error error, const char *description)
+    : std::system_error(sd_bus_error_get_errno(&error),
+                        std::generic_category()),
+      error(error)
+{
+    populateMessage(description);
+}
+
+SdBusError::~SdBusError()
+{
+    sd_bus_error_free(&error);
+}
+
+const char* SdBusError::name() const noexcept
+{
+    return error.name;
+}
+
+const char* SdBusError::description() const noexcept
+{
+    return description_internal;
+}
+
+const char* SdBusError::what() const noexcept
+{
+    return full_message.c_str();
+}
+
+void SdBusError::populateMessage(const char *description)
+{
+    full_message = error.name;
+    full_message += ": ";
+    const size_t description_start = full_message.length();
+    full_message += description;
+    description_internal = full_message.c_str() + description_start;
+}
+
 const char* InvalidEnumString::name() const noexcept
 {
     return errName;

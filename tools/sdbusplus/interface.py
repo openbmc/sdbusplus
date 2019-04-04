8515eae7adcb9811ba271d05d82f66d48b1df082
William A. Kennington III <wak@google.com>
2019-04-03 20:41:57 -0700

    sdbus++: Generate includes for enum references
    It is common for enums to be shared across different dbus definitions.
    This change adds the functionality to automatically generate the correct
    includes for the server.hpp files.
    
    Change-Id: Ic5b8df55b58479d18dd4aee20ead9c05b867b968
    Signed-off-by: William A. Kennington III <wak@google.com>

--- tools/sdbusplus/interface.py
+++ tools/sdbusplus/interface.py
@@ -32,6 +32,26 @@ class Interface(NamedElement, Renderer):
 
         super(Interface, self).__init__(**kwargs)
 
+    def enum_includes(self, l):
+        includes = []
+        for e in l:
+            es = e.enum_namespace(self.name).split('::')
+            # Skip empty, non-enum values and self references like '::'
+            if len(es) < 2:
+                continue
+            # All elements will be formatted (x::)+
+            # If the requested enum is xyz.openbmc_project.Network.IP.Protocol
+            # for a server_* configuration, the enum_namespace will be
+            # xyz::openbmc_project::Network::server::IP:: and we need to
+            # convert to xyz/openbmc_project/Network/IP/server.hpp
+            es.pop()  # Remove trailing empty element
+            e_class = es.pop() # Remove class name
+            e_type = es.pop()  # Remove injected type namespace
+            es.append(e_class)
+            es.append(e_type)
+            includes.append('/'.join(es) + '.hpp')
+        return includes
+
     def markdown(self, loader):
         return self.render(loader, "interface.mako.md", interface=self)
 

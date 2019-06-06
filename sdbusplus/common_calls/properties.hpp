04e852a2cdb305eff1bb1f3caa6375b25502137f
William A. Kennington III <wak@google.com>
2019-06-05 20:41:41 -0700

    WIP common_calls: Add commonly used dbus property method calls
    Change-Id: I1c8847f1d3be1e916be4b8873840b81584a95fd1
    Signed-off-by: William A. Kennington III <wak@google.com>

---
+++ sdbusplus/common_calls/properties.hpp
@@ -0,0 +1,83 @@
+#pragma once
+#include <map>
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/typed.hpp>
+#include <string>
+#include <utility>
+#include <variant>
+
+namespace sdbusplus
+{
+namespace common_calls
+{
+namespace properties
+{
+
+template <typename Property>
+auto get(bus::bus& bus, const char* service, const char* object)
+{
+    using Req = typed::Types<std::string, std::string>;
+    using Rsp = typed::Types<std::variant<Property>>;
+    return typed::MethodCall<Req, Rsp>(bus.new_method_call(
+        service, object, "org.freedesktop.DBus.Properties", "Get"));
+}
+
+template <typename Property>
+Property get(bus::bus& bus, const char* service, const char* object,
+             const char* interface, const char* property)
+{
+    std::variant<Property> ret;
+    get<Property>(bus, service, object)
+        .append(interface, property)
+        .call()
+        .read(ret);
+    return std::move(std::get<Property>(ret));
+}
+
+template <typename Property>
+auto set(bus::bus& bus, const char* service, const char* object)
+{
+    using Req = typed::Types<std::string, std::string, std::variant<Property>>;
+    using Rsp = typed::Types<>;
+    return typed::MethodCall<Req, Rsp>(bus.new_method_call(
+        service, object, "org.freedesktop.DBus.Properties", "Set"));
+}
+
+template <typename Property>
+void set(bus::bus& bus, const char* service, const char* object,
+         const char* interface, const char* property, Property&& value)
+{
+    set<Property>(bus, service, object)
+        .append(interface, property)
+        .append(std::variant<Property>(std::forward<Property>(value)))
+        .call();
+}
+
+template <typename... Properties>
+using PropertyMap = std::map<std::string, std::variant<Properties...>>;
+
+template <typename... Properties>
+auto getAll(bus::bus& bus, const char* service, const char* object)
+{
+    using Req = typed::Types<std::string>;
+    using Rsp = typed::Types<PropertyMap<Properties...>>;
+    return typed::MethodCall<Req, Rsp>(bus.new_method_call(
+        service, object, "org.freedesktop.DBus.Properties", "GetAll"));
+}
+
+template <typename... Properties>
+std::map<std::string, std::variant<Properties...>>
+    getAll(bus::bus& bus, const char* service, const char* object,
+           const char* interface)
+{
+    PropertyMap<Properties...> ret;
+    getAll<Properties...>(bus, service, object)
+        .append(interface)
+        .call()
+        .read(ret);
+    return ret;
+}
+
+} // namespace properties
+} // namespace common_calls
+} // namespace sdbusplus

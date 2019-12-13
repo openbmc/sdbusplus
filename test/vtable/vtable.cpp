f641241e5d2c4c3a7b087921cbef51f8428136ef
Lei YU <mine260309@gmail.com>
2019-12-13 14:48:03 +0800

    Fix vtable CI error
    The CI reports failure in VtableTest.SameContent, where the case is
    comparing the binary of example vtables constructed from sdbusplus and
    macros defined in systemd.
    
    The root cause has two parts.
    
    Part I:
    When systemd is updated, the members in struct
    sd_bus_vtable is updated, e.g.
    * From v239 to v242, sd_bus_vtable.x.start is updated from
          struct {
                  size_t element_size;
          } start;
      to:
          struct {
                  size_t element_size;
                  uint64_t features;
          } start;
      and sd_bus_vtable.x.method is updated from
          struct {
                  const char *member;
                  const char *signature;
                  const char *result;
                  sd_bus_message_handler_t handler;
                  size_t offset;
          } method;
      to:
          struct {
                  const char *member;
                  const char *signature;
                  const char *result;
                  sd_bus_message_handler_t handler;
                  size_t offset;
                  const char *names;
          } method;
    * From v242 to v243, sd_bus_vtable.x.start is updated to
          struct {
                  size_t element_size;
                  uint64_t features;
                  const unsigned *vtable_format_reference;
          } start;
    
    The code in vtable.hpp only assign the members in v239, the new
    members are intialized with 0, so it differs from the vtable
    constructed from macros defined by systemd.
    
    The fix is to use macros from systemd in vtable.hpp as well,
    which is suggested by systemd:
    
    > Please do not initialize this structure directly, use the
    > macros below instead
    
    Part II:
    The `const char *names` in struct method and signal introduced
    between systemd v239 and v242 is a pointer to const strings.
    
    By default they are "empty" strings, but there is no gurantee
    that the compiler will use the same pointer for the same string.
    
    So the test case can not assume the binaries are the same for
    two vtables even they are constructed with the same parameters.
    
    Update the test case to handle the comprae of "names"
    specifically to fix this issue.
    
    Tested: Verify the CI passes.
    
    Change-Id: I9e94ff16075dd3f12d73e96438c0d864203bdcf4
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- test/vtable/vtable.cpp
+++ test/vtable/vtable.cpp
@@ -24,7 +24,49 @@ TEST(VtableTest, SameSize)
     ASSERT_EQ(sizeof(example), example2_size);
 }
 
+constexpr bool operator==(const sd_bus_vtable& t1, const sd_bus_vtable& t2)
+{
+    constexpr size_t sizeOfMethodWithoutNames =
+        8 /* sizeof (type + flags) */ + sizeof(t1.x.method.member) +
+        sizeof(t1.x.method.signature) + sizeof(t1.x.method.result) +
+        sizeof(t1.x.method.handler) + sizeof(t1.x.method.offset);
+    constexpr size_t sizeOfSignalWithoutNames = 8 /* sizeof (type + flags) */ +
+                                                sizeof(t1.x.method.member) +
+                                                sizeof(t1.x.method.signature);
+
+    if (t1.type != t2.type)
+    {
+        return false;
+    }
+
+    switch (t1.type)
+    {
+        case _SD_BUS_VTABLE_START:
+        case _SD_BUS_VTABLE_END:
+        case _SD_BUS_VTABLE_PROPERTY:
+        case _SD_BUS_VTABLE_WRITABLE_PROPERTY:
+            return memcmp(&t1, &t2, sizeof(t1)) == 0;
+            break;
+        case _SD_BUS_VTABLE_METHOD:
+            // The sd_bus_vtable.x.method.names is a pointer to a const char*,
+            // that is not guranteed to be the same address for the same string.
+            return memcmp(&t1, &t2, sizeOfMethodWithoutNames) == 0 &&
+                   strcmp(t1.x.method.names, t2.x.method.names) == 0;
+            break;
+        case _SD_BUS_VTABLE_SIGNAL:
+            // The sd_bus_vtable.x.signal.names is a pointer to a const char*,
+            // that is not guranteed to be the same address for the same string.
+            return memcmp(&t1, &t2, sizeOfSignalWithoutNames) == 0 &&
+                   strcmp(t1.x.signal.names, t2.x.signal.names) == 0;
+            break;
+    }
+    return false;
+}
+
 TEST(VtableTest, SameContent)
 {
-    ASSERT_EQ(0, memcmp(example, example2, example2_size));
+    for (size_t i = 0; i < sizeof(example) / sizeof(example[0]); ++i)
+    {
+        EXPECT_EQ(example[i], example2[i]);
+    }
 }

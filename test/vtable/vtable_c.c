d35c85239a85a101e866a7bd60aff4f40bf77401
Lei YU <mine260309@gmail.com>
2019-12-13 14:48:03 +0800

    Fix vtable CI error
    The CI reports failure in VtableTest.SameContent, where the case is
    comparing the binary of example vtables constructed from sdbusplus and
    macros defined in systemd.
    
    The root cause has two parts.
    
    Part I:
    When systemd is updated, the members in struct
    sd_bus_vtable is updated, e.g.
    * From v239 to v242, sd_bus_vtable.x.start is updated from
          struct {
                  size_t element_size;
          } start;
      to:
          struct {
                  size_t element_size;
                  uint64_t features;
          } start;
      and sd_bus_vtable.x.method is updated from
          struct {
                  const char *member;
                  const char *signature;
                  const char *result;
                  sd_bus_message_handler_t handler;
                  size_t offset;
          } method;
      to:
          struct {
                  const char *member;
                  const char *signature;
                  const char *result;
                  sd_bus_message_handler_t handler;
                  size_t offset;
                  const char *names;
          } method;
    * From v242 to v243, sd_bus_vtable.x.start is updated to
          struct {
                  size_t element_size;
                  uint64_t features;
                  const unsigned *vtable_format_reference;
          } start;
    
    The code in vtable.hpp only assign the members in v239, the new
    members are intialized with 0, so it differs from the vtable
    constructed from macros defined by systemd.
    
    The fix is to use macros from systemd in vtable.hpp as well,
    which is suggested by systemd:
    
    > Please do not initialize this structure directly, use the
    > macros below instead
    
    Part II:
    The `const char *names` in struct method and signal introduced
    between systemd v239 and v242 is a pointer to const strings.
    
    By default they are "empty" strings, but there is no guarantee
    that the compiler will use the same pointer for the same string.
    
    So the test case can not assume the binaries are the same for
    two vtables even they are constructed with the same parameters.
    
    Update the test case to use real `const char*` and handler/get/set
    functions and check each member of the struct instead of comparing the
    binary data by memcmp.
    
    Tested: Verify the CI passes.
    
    Change-Id: I9e94ff16075dd3f12d73e96438c0d864203bdcf4
    Signed-off-by: Lei YU <mine260309@gmail.com>

--- test/vtable/vtable_c.c
+++ test/vtable/vtable_c.c
@@ -1,16 +1,50 @@
 #include <systemd/sd-bus.h>
 
+#define UNUSED(x) (void)(x)
+
+int test_handler(sd_bus_message* m, void* userdata, sd_bus_error* ret_error)
+{
+    UNUSED(m);
+    UNUSED(userdata);
+    UNUSED(ret_error);
+    return 0;
+}
+int test_get(sd_bus* bus, const char* path, const char* interface,
+             const char* property, sd_bus_message* reply, void* userdata,
+             sd_bus_error* ret_error)
+{
+    UNUSED(bus);
+    UNUSED(path);
+    UNUSED(interface);
+    UNUSED(property);
+    UNUSED(reply);
+    UNUSED(userdata);
+    UNUSED(ret_error);
+    return 0;
+}
+int test_set(sd_bus* bus, const char* path, const char* interface,
+             const char* property, sd_bus_message* value, void* userdata,
+             sd_bus_error* ret_error)
+{
+    UNUSED(bus);
+    UNUSED(path);
+    UNUSED(interface);
+    UNUSED(property);
+    UNUSED(value);
+    UNUSED(userdata);
+    UNUSED(ret_error);
+    return 0;
+}
+
+typedef int (*sd_bus_message_handler_t)(sd_bus_message* m, void* userdata,
+                                        sd_bus_error* ret_error);
 const sd_bus_vtable example2[] = {
     SD_BUS_VTABLE_START(0),
-    SD_BUS_METHOD((const char*)1, (const char*)2, (const char*)3,
-                  (sd_bus_message_handler_t)4, 0),
-    SD_BUS_SIGNAL((const char*)5, (const char*)6, 0),
-    SD_BUS_PROPERTY((const char*)7, (const char*)8, (sd_bus_property_get_t)9, 0,
-                    SD_BUS_VTABLE_PROPERTY_CONST),
-    SD_BUS_WRITABLE_PROPERTY((const char*)10, (const char*)11,
-                             (sd_bus_property_get_t)12,
-                             (sd_bus_property_set_t)13, 0, 0),
-    SD_BUS_PROPERTY((const char*)14, (const char*)15, NULL, 16, 0),
+    SD_BUS_METHOD("1", "2", "3", &test_handler, 0),
+    SD_BUS_SIGNAL("5", "6", 0),
+    SD_BUS_PROPERTY("7", "8", &test_get, 0, SD_BUS_VTABLE_PROPERTY_CONST),
+    SD_BUS_WRITABLE_PROPERTY("10", "11", &test_get, &test_set, 0, 0),
+    SD_BUS_PROPERTY("14", "15", NULL, 16, 0),
     SD_BUS_VTABLE_END,
 };
 

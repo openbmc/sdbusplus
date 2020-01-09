96c1830e839ea0bd7a9524de365a32cc6bc47b81
Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
2020-01-09 17:21:58 +0100

    Fix exception handling inside asio coroutine method_handler
    By default sdbusplus catches exceptions from method handlers and
    translates them to proper error message reply.
    See asio/object_server.hpp:method_handler for reference.
    
    This behavior is not kept when coroutine is used. If yield_context
    was not yet used - exception is caught properly. In other cases
    exception was 'leaking' and left unhandled, effectively crashing app.
    
    Following change introduces exception handling similar to previously
    mentioned implementation. In case of exception thrown from handler
    in coroutine it is caught and translated to proper D-Bus error reply.
    
    Signed-off-by: Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
    Change-Id: Id86b77577a2547f999fa1a10ca4316ec0a9ac2d1

--- sdbusplus/asio/object_server.hpp
+++ sdbusplus/asio/object_server.hpp
@@ -202,7 +202,26 @@ class coroutine_method_instance : public callback
         boost::asio::spawn(
             io_, [this, b = std::move(b)](boost::asio::yield_context yield) {
                 message::message mcpy{std::move(b)};
-                expandCall(yield, mcpy);
+                std::optional<message::message> err{};
+
+                try
+                {
+                    expandCall(yield, mcpy);
+                }
+                catch (sdbusplus::exception::SdBusError& e)
+                {
+                    // Catch D-Bus error explicitly called by method handler
+                    err = mcpy.new_method_errno(e.get_errno());
+                }
+                catch (...)
+                {
+                    err = mcpy.new_method_errno(-EIO);
+                }
+
+                if (err)
+                {
+                    err->method_return();
+                }
             });
         return 1;
     }

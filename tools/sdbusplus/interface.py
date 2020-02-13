a9f4d88e49146201569e0bd57c56ae9e9e983da9
Lei YU <mine260309@gmail.com>
2020-02-13 16:26:46 +0800

    sdbus++: Generate common header for an interface
    By "common-header" option, sdbus++ could generate a common header file
    that provides the public information that could be shared by all the
    interfaces, e.g. the interface string.
    
    Currently it only includes the interface name's string in the header
    file, it could be extended in the future.
    
    The common header could be merged together for all the interfaces, so
    that an interface library could provide a single header to represent all
    the interface strings.
    
    Usage:
    
     sdbus++ -r $(srcdir) -t $(top_builddir)/tools/sdbusplus/templates \
        interface common-header <interface>
    
    Tested: Generate the header in example and use static_assert to verify
            it is the same as the interface defined in the interface calss.
    
    Signed-off-by: Lei YU <mine260309@gmail.com>
    Change-Id: Idafc4724efa88a2dfd37e3f7735732c45171fd88

--- tools/sdbusplus/interface.py
+++ tools/sdbusplus/interface.py
@@ -60,3 +60,6 @@ class Interface(NamedElement, Renderer):
 
     def server_cpp(self, loader):
         return self.render(loader, "interface.mako.server.cpp", interface=self)
+
+    def common_header(self, loader):
+        return self.render(loader, "common_include.mako.hpp", interface=self)

d113532d35191ba375df3a6e33b4afe06e21b5e7
Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
2020-03-25 20:25:05 +0100

    Added register_signal method to sdbusplus::asio::dbus_interface
    Signals, just like other interface members, should be listed on
    interface v-table. This change introduces ability to expose
    information about signal defined by D-Bus interface.
    
    Tested by calling function in following ways:
    iface->register_signal("Empty");                           // -
    iface->register_signal<void>("EmptyTwo");                  // -
    iface->register_signal<double,std::string>("TwoElems");    // ds
    iface->register_signal<std::tuple<int, double>>("Struct"); // (id)
    iface->register_signal<std::vector<int>>("Array");         // ai
    
    Signed-off-by: Adrian Ambrożewicz <adrian.ambrozewicz@linux.intel.com>
    Change-Id: I477ebdee0c4eb814e0c18987c96fe7d7c1eb90e6

--- sdbusplus/asio/object_server.hpp
+++ sdbusplus/asio/object_server.hpp
@@ -518,6 +518,37 @@ class dbus_interface
         return false;
     }
 
+    bool register_signal(const std::string& name)
+    {
+        return register_signal<void>(name);
+    }
+
+    template <typename... SignalSignature>
+    bool register_signal(const std::string& name)
+    {
+        if (initialized_)
+        {
+            return false;
+        }
+        else if (name.find_first_not_of(PropertyNameAllowedCharacters) !=
+                 std::string::npos)
+        {
+            return false;
+        }
+
+        static const auto signature = utility::tuple_to_array(
+            message::types::type_id<SignalSignature...>());
+
+        auto [itr, inserted] = signalNames_.insert(name);
+        if (!inserted)
+        {
+            return false;
+        }
+
+        vtable_.emplace_back(vtable::signal(itr->c_str(), signature.data()));
+        return true;
+    }
+
 #ifdef __cpp_if_constexpr
     template <typename CallbackType>
     bool register_method(const std::string& name, CallbackType&& handler)
@@ -750,6 +781,7 @@ class dbus_interface
     std::string name_;
     std::list<std::string> propertyNames_;
     std::list<std::string> methodNames_;
+    std::set<std::string> signalNames_;
     boost::container::flat_map<std::string, std::unique_ptr<callback>>
         callbacksGet_;
     boost::container::flat_map<std::string, std::unique_ptr<callback_set>>

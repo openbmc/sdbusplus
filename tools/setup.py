c0f1cd2fdcdec4168ac3d08d72098afbd53eee45
Patrick Williams <patrick@stwcx.xyz>
2020-05-15 17:41:29 -0500

    build: add setuptools install script
    Add a new setuptools install script which will allow us to eliminate
    the autotools-based python install in some environments, such as Yocto.
    This will reduce the recipe complexity and allow future work towards
    meson-based build.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I23af49db2ba2245a5a44cfa5bc50a4734e7ca89c

---
+++ tools/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+setup(
+    name="sdbusplus",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["inflection", "mako", "pyyaml"],
+    scripts=["sdbus++"],
+    package_data={"sdbusplus": ["templates/*.mako.*"]},
+    url="http://github.com/openbmc/sdbusplus",
+    classifiers=["License :: OSI Approved :: Apache Software License"],
+)

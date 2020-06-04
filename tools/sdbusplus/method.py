3e3a665a2ba7938264b5f6501c4cec79ccd1daec
Waqar Hameed <waqarh@axis.com>
2020-06-04 10:51:18 +0200

    Add support for vtable flags for methods
    This adds support for specifying the vtable flags
    SD_BUS_VTABLE_DEPRECATED, SD_BUS_VTABLE_HIDDEN,
    SD_BUS_VTABLE_UNPRIVILEGED and SD_BUS_VTABLE_METHOD_NO_REPLY for
    methods. The list of flags in the YAML file will be ORed together.
    
    Change-Id: I7762ffb15a1483760f3b7e0ea02e80fd52ebd9b9
    Signed-off-by: Waqar Hameed <waqarh@axis.com>

--- tools/sdbusplus/method.py
+++ tools/sdbusplus/method.py
@@ -9,6 +9,8 @@ class Method(NamedElement, Renderer):
             [Property(**p) for p in kwargs.pop('parameters', [])]
         self.returns = \
             [Property(**r) for r in kwargs.pop('returns', [])]
+        self.flags = kwargs.pop('flags', [])
+        self.cpp_flags = self.or_cpp_flags(self.flags)
         self.errors = kwargs.pop('errors', [])
 
         super(Method, self).__init__(**kwargs)
@@ -19,3 +21,19 @@ class Method(NamedElement, Renderer):
     def cpp_prototype(self, loader, interface, ptype):
         return self.render(loader, "method.prototype.hpp.mako", method=self,
                            interface=interface, ptype=ptype, post=str.rstrip)
+
+    def or_cpp_flags(self, flags):
+        """Return the corresponding ORed cpp flags."""
+        flags_dict = {"deprecated": "vtable::common_::deprecated",
+                      "hidden": "vtable::common_::hidden",
+                      "unprivileged": "vtable::common_::unprivileged",
+                      "no_reply": "vtable::method_::no_reply"}
+
+        cpp_flags = []
+        for flag in flags:
+            try:
+                cpp_flags.append(flags_dict[flag])
+            except KeyError:
+                raise ValueError("Invalid flag \"{}\"".format(flag))
+
+        return " | ".join(cpp_flags)

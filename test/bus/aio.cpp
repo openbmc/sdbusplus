56fcdec5cb579c9c36875f3f1358f1b2e375004e
Andrew Geissler <geissonator@yahoo.com>
2020-06-08 21:33:52 -0500

    test: add simple aio case
    Add a simple test case to confirm we can successfully create an
    asio::object_server.
    
    Signed-off-by: Andrew Geissler <geissonator@yahoo.com>
    Change-Id: I3cee879850a41b2dd8750e724c6e65c00aded308
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>

---
+++ test/bus/aio.cpp
@@ -0,0 +1,16 @@
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/bus.hpp>
+
+#include <gtest/gtest.h>
+
+constexpr auto this_name = "xyz.openbmc_project.sdbusplus.test.Aio";
+
+TEST(AioTest, BasicTest)
+{
+    boost::asio::io_service io;
+    auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
+    EXPECT_NE(nullptr, systemBus);
+
+    systemBus->request_name(this_name);
+    sdbusplus::asio::object_server objectServer(systemBus);
+}

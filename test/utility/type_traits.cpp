33ff3e1db632de47d8fd01392d5e3c60de88921f
Krzysztof Grobelny <krzysztof.grobelny@intel.com>
2020-09-02 14:49:01 +0200

    Added utility functions getAllProperties and unpackProperties
    Tested:
      - Added example to verify that functions work correctly
      - Added new unit tests that are passing
      - All other tests are still passing after this change
      - Added handling of new type (std::monostate) which can
        be used as first type in variant to represent that none
        of the other types was matched
    
    Change-Id: Ic8e7c8d3116d64b94be37147ae8a80ebb5d3811d
    Signed-off-by: Krzysztof Grobelny <krzysztof.grobelny@intel.com>

--- test/utility/type_traits.cpp
+++ test/utility/type_traits.cpp
@@ -2,7 +2,7 @@
 
 #include <type_traits>
 
-#include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 namespace
 {
@@ -24,6 +24,72 @@ TEST(TypeTraits, Basic)
 
     static_assert(is_same<char[100], array_to_ptr_t<int, char[100]>>::value,
                   "array_to_ptr_t<int, char[100]> != char[100]");
+}
+
+template <class T, class U>
+void assertWrapper(T&& t, U&& u, std::string_view message)
+{
+    ASSERT_THAT(t,
+                u)
+        << message; // ASSERT macros doesn't like commas when templates are used
+}
+
+TEST(TypeTraits, HasMemberFind)
+{
+    using sdbusplus::utility::has_member_find_v;
+    using namespace testing;
+
+    assertWrapper(has_member_find_v<std::map<std::string, int>>, Eq(true),
+                  "std::map");
+    assertWrapper(has_member_find_v<std::vector<std::pair<std::string, int>>>,
+                  Eq(false), "std::vector");
+
+    struct Foo
+    {
+        using value_type = std::pair<int, int>;
+
+        void find(std::tuple_element_t<0, value_type>)
+        {}
+    };
+
+    struct Bar
+    {};
+
+    assertWrapper(has_member_find_v<Foo>, Eq(true), "Foo");
+    assertWrapper(has_member_find_v<Foo&>, Eq(true), "Foo&");
+    assertWrapper(has_member_find_v<const Foo&>, Eq(true), "const Foo&");
+
+    assertWrapper(has_member_find_v<Bar>, Eq(false), "Bar");
+}
+
+TEST(TypeTraits, HasMemberContains)
+{
+    using sdbusplus::utility::has_member_contains_v;
+    using namespace testing;
+
+    // std::map has member_contains from c++20
+    assertWrapper(has_member_contains_v<std::map<std::string, int>>, Eq(false),
+                  "std::map");
+    assertWrapper(
+        has_member_contains_v<std::vector<std::pair<std::string, int>>>,
+        Eq(false), "std::vector");
+
+    struct Foo
+    {
+        using value_type = std::pair<int, int>;
+
+        void contains(std::tuple_element_t<0, value_type>)
+        {}
+    };
+
+    struct Bar
+    {};
+
+    assertWrapper(has_member_contains_v<Foo>, Eq(true), "Foo");
+    assertWrapper(has_member_contains_v<Foo&>, Eq(true), "Foo&");
+    assertWrapper(has_member_contains_v<const Foo&>, Eq(true), "const Foo&");
+
+    assertWrapper(has_member_contains_v<Bar>, Eq(false), "Bar");
 }
 
 } // namespace

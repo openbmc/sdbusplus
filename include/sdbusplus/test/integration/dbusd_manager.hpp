eefe55b13498f24c1e723c99f5e6faa1c1fd4f47
Ramin Izadpanah <iramin@google.com>
2020-11-17 18:39:09 +0000

    integration test: Add D-Bus daemon manager
    Responsible for managing dbus-daemon specifically.
    
    It forks a new dbus-daemon and sets the variables.
    
    If dbus-daemon is not available, shows a help message.
    
    Signed-off-by: Ramin Izadpanah <iramin@google.com>
    Change-Id: I876330e158746ab7d7e1231f7e5b399e15ddcc84

---
+++ include/sdbusplus/test/integration/dbusd_manager.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <sdbusplus/test/integration/daemon_manager.hpp>
+
+#include <string>
+
+namespace sdbusplus
+{
+
+namespace test
+{
+
+namespace integration
+{
+
+
+class DBusDaemon : public Daemon
+{
+    public:
+        DBusDaemon(const DBusDaemon&) = delete;
+        DBusDaemon& operator=(const DBusDaemon&) = delete;
+        DBusDaemon(DBusDaemon&&) = delete;
+        DBusDaemon& operator=(DBusDaemon&&) = delete;
+        
+        DBusDaemon(const std::string dbusConfPath,
+                   const std::string stdoutPath,
+                   bool useSystemBus = true);
+        
+    protected:
+        void postStart() override;
+
+        void postTerminate() override;
+
+        std::string getPathNotFoundHelpMsg() override;
+
+    private:   
+
+        void setForkedDaemonPid(const char* pidStr);
+
+        bool isDaemonTerminated();
+
+        void forceKillDaemon();
+
+        void setBusAddressEnvVariable(const char* address);
+        
+        void unsetBusAddressEnvVariable();
+        
+        bool useSystemBus;
+};
+
+
+} // namespace integration
+} // namespace test
+} // namespace sdbusplus

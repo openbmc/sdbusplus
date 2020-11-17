61264bef599ccf041fd17587e16412201095c1bd
Ramin Izadpanah <iramin@google.com>
2020-11-17 18:47:38 +0000

    integration test: Add base class for mock services
    Common functionalities used by mock services are expected to be
    placed here.
    
    Each service has a worker thread that starts when the user calls
    start() on the service.
    The worker thread executes the run function.
    Active services can override the proceed function to customize
    the thread behavior.
    
    Signed-off-by: Ramin Izadpanah <iramin@google.com>
    Change-Id: I5b7338eabe85e7f5162a3b1492bd56a854e6a48b

---
+++ src/sdbusplus/test/integration/mock_service.cpp
@@ -0,0 +1,121 @@
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/test/integration/mock_object.hpp>
+#include <sdbusplus/test/integration/mock_service.hpp>
+#include <sdbusplus/test/integration/test_timer.hpp>
+
+#include <chrono> // std::chrono::seconds
+#include <memory>
+#include <string>
+#include <thread>
+
+namespace sdbusplus
+{
+
+namespace test
+{
+
+namespace integration
+{
+
+MockService::MockService(std::string serviceName,
+                         std::shared_ptr<PrivateBus> privateBus,
+                         SdBusDuration microsecondsToRun) :
+    started(false),
+    name(serviceName), mockBus(privateBus), activeDuration(microsecondsToRun)
+{}
+
+bool MockService::hasObjects()
+{
+    return !objectRepo.empty();
+}
+
+bool MockService::isStarted()
+{
+    return started;
+}
+
+void MockService::addObject(std::shared_ptr<MockObject> obj)
+{
+    if (!hasObjects())
+    {
+        mainObjectPath = obj->getPath();
+    }
+    objectRepo.emplace(obj->getPath(), obj);
+    if (isStarted())
+    {
+        std::lock_guard<std::mutex> lock(afterStartPendingObjectsMutex);
+        afterStartPendingObjects.push_back(obj->getPath());
+    }
+}
+
+void MockService::registerService()
+{
+    mockBus->registerService(name);
+}
+
+MockService::~MockService()
+{
+    stop();
+    mockBus->unregisterService(name);
+}
+
+void MockService::start()
+{
+    if (!isStarted())
+    {
+        shouldWork = true;
+        worker = std::make_shared<std::thread>(&MockService::run, this);
+    }
+}
+
+void MockService::stop()
+{
+    shouldWork = false;
+    if (worker->joinable())
+    {
+        worker->join();
+    }
+    started = false;
+}
+
+void MockService::run()
+{
+    registerService();
+    bus = mockBus->getBus(name);
+    startObjects();
+    started = true;
+    Timer test;
+    auto duration = test.duration();
+    while (shouldWork && duration < activeDuration)
+    {
+        bus->process_discard(); // discard any unhandled messages
+        bus->wait(activeDuration - duration);
+        proceed();
+        duration = test.duration();
+    }
+}
+
+void MockService::startObjects()
+{
+    for (auto const& [path, object] : objectRepo)
+    {
+        object->start(*bus);
+    }
+}
+
+void MockService::proceed()
+{
+    if (!afterStartPendingObjects.empty())
+    {
+        std::lock_guard<std::mutex> lock(afterStartPendingObjectsMutex);
+        for (const auto& path : afterStartPendingObjects)
+        {
+            objectRepo[path]->start(*bus);
+        }
+        afterStartPendingObjects.clear();
+    }
+}
+
+} // namespace integration
+} // namespace test
+} // namespace sdbusplus

c51f2a14a6a2eeb4547d94da219b5cc1d86f8fd8
Ramin Izadpanah <iramin@google.com>
2020-11-17 18:40:51 +0000

    integration test: Add timer to control timed loops
    Some loop-based procedures should run for a certain amount of time.
    
    Signed-off-by: Ramin Izadpanah <iramin@google.com>
    Change-Id: Ia4ca13d0d7db6b5520640bc8fa888552fd4a4221

---
+++ include/sdbusplus/test/integration/test_timer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+
+#include <chrono>
+
+namespace sdbusplus
+{
+
+namespace test
+{
+
+namespace integration
+{
+
+class Timer
+{
+  public:
+    Timer() : start(std::chrono::steady_clock::now()){};
+
+    SdBusDuration duration()
+    {
+        return std::chrono::duration_cast<SdBusDuration>(
+            std::chrono::steady_clock::now() - start);
+    };
+
+  private:
+    std::chrono::steady_clock::time_point start;
+};
+
+} // namespace integration
+} // namespace test
+} // namespace sdbusplus

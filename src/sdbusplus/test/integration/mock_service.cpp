0eed6ad4f5b416290b8f327b90b258a1b096f5f9
Ramin Izadpanah <iramin@google.com>
2020-11-17 18:47:38 +0000

    integration test: Add base class for mock services
    Common functionalities used by mock services are expected to be
    placed here.
    
    The generic base class is for services that are not active and mostly
     respond to signals.
    Active mock service is for services that are actively working during the test.
    
    Signed-off-by: Ramin Izadpanah <iramin@google.com>
    Change-Id: I5b7338eabe85e7f5162a3b1492bd56a854e6a48b

---
+++ src/sdbusplus/test/integration/mock_service.cpp
@@ -0,0 +1,104 @@
+#include <sdbusplus/test/integration/mock_service.hpp>
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/test/integration/mock_object.hpp>
+#include <sdbusplus/test/integration/test_timer.hpp>
+
+#include <chrono>         // std::chrono::seconds
+#include <memory>
+#include <string>
+#include <thread>
+
+
+namespace sdbusplus
+{
+
+namespace test
+{
+
+namespace integration
+{
+
+MockService::MockService(std::shared_ptr<sdbusplus::bus::bus> inBus,
+                         std::string serviceName):
+started(false),
+name(serviceName),
+bus(*inBus)
+{
+    registerService();
+}
+
+sdbusplus::bus::bus& MockService::getBus()
+{
+    return bus;
+}
+
+bool MockService::hasObjects()
+{
+    return !objectRepo.empty();
+}
+
+bool MockService::isStarted()
+{
+    return started;
+}
+
+void MockService::addObject(std::shared_ptr<MockObject> obj)
+{
+    if (!hasObjects())
+    {
+        mainObjectPath = obj->getPath();
+    }
+    objectRepo.emplace(obj->getPath(), obj);
+
+}
+
+void MockService::registerService()
+{
+    bus.request_name(name.c_str());
+}
+
+
+MockServiceActive::MockServiceActive(std::shared_ptr<sdbusplus::bus::bus> inBus,
+                                     std::string serviceName,
+                                     SdBusDuration microsecondsToRun,
+                                     SdBusDuration microsecondsBetweenSteps):
+MockService(inBus, serviceName),
+activeDuration(microsecondsToRun),
+timeBetweenSteps(microsecondsBetweenSteps),
+worker(&MockServiceActive::run, this)
+{
+}
+
+MockServiceActive::~MockServiceActive()
+{
+    if (worker.joinable())
+    {
+        worker.join();
+    }
+}
+
+void MockServiceActive::run()
+{
+    if (isStarted())
+    {
+        Timer test;
+        auto duration = test.duration();
+        while (duration < activeDuration)
+        {
+            proceed();
+            duration = test.duration();
+        }
+    }
+}
+
+void MockServiceActive::proceed()
+{
+    std::this_thread::sleep_for(timeBetweenSteps);
+}
+
+
+
+} // namespace integration
+} // namespace test
+} // namespace sdbusplus

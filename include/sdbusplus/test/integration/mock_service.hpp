c74940b0ef2b251d46ed7ad9156f81fec0d26c99
Ramin Izadpanah <iramin@google.com>
2020-11-17 18:47:38 +0000

    integration test: Add base class for mock services
    Common functionalities used by mock services are expected to be
    placed here.
    
    Each service has a worker thread that starts when the user calls
    start() on the service.
    The worker thread executes the run function.
    Active services can override the proceed function to customize
    the thread behavior.
    
    A unit test is added that tests both mock service and mock objects.
    example_mock_service implements mock services/objects for the
    interface specified by net/example/Software/Updater.interface.yaml.
    
    Signed-off-by: Ramin Izadpanah <iramin@google.com>
    Change-Id: I5b7338eabe85e7f5162a3b1492bd56a854e6a48b

---
+++ include/sdbusplus/test/integration/mock_service.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/test/integration/mock_object.hpp>
+#include <sdbusplus/test/integration/private_bus.hpp>
+
+#include <atomic>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <unordered_map>
+
+using namespace std::literals::chrono_literals;
+
+namespace sdbusplus
+{
+
+namespace test
+{
+
+namespace integration
+{
+
+class MockService
+{
+  public:
+    MockService(std::string serviceName, std::shared_ptr<PrivateBus> privateBus,
+                SdBusDuration microsecondsToRun);
+
+    virtual ~MockService();
+
+    void start();
+
+    void stop();
+
+    bool isStarted();
+
+    std::unordered_map<std::string, std::shared_ptr<MockObject>> objectRepo;
+
+  protected:
+    void addObject(std::shared_ptr<MockObject> obj);
+
+    bool hasObjects();
+
+    std::string mainObjectPath;
+    std::atomic<bool> started;
+    virtual void run();
+    virtual void proceed();
+
+  private:
+    void registerService();
+    void startObjects();
+
+    std::string name;
+    std::shared_ptr<PrivateBus> mockBus;
+    SdBusDuration activeDuration;
+    std::shared_ptr<std::thread> worker;
+    std::shared_ptr<sdbusplus::bus::bus> bus;
+
+    std::vector<std::string> afterStartPendingObjects;
+    std::mutex afterStartPendingObjectsMutex;
+
+    bool shouldWork;
+};
+
+} // namespace integration
+} // namespace test
+} // namespace sdbusplus

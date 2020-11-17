65c8825269beca9c836619589c6c70f86b17c7fe
Ramin Izadpanah <iramin@google.com>
2020-11-17 18:47:38 +0000

    integration test: Add base class for mock services
    Common functionalities used by mock services are expected to be
    placed here.
    
    The generic base class is for services that are not active and mostly
     respond to signals.
    Active mock service is for services that are actively working during the test.
    
    Signed-off-by: Ramin Izadpanah <iramin@google.com>
    Change-Id: I5b7338eabe85e7f5162a3b1492bd56a854e6a48b

---
+++ include/sdbusplus/test/integration/mock_service.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <sdbusplus/bus.hpp>
+#include <sdbusplus/test/integration/mock_object.hpp>
+
+#include <memory>
+#include <string>
+#include <thread>
+#include <unordered_map>
+
+using namespace std::literals::chrono_literals;
+
+namespace sdbusplus
+{
+
+namespace test
+{
+
+namespace integration
+{
+
+class MockService
+{
+    public:
+        MockService(std::shared_ptr<sdbusplus::bus::bus> inBus,
+                    std::string serviceName);
+
+        virtual ~MockService() = default;
+
+        sdbusplus::bus::bus& getBus();
+
+        bool isStarted();
+
+        std::unordered_map<std::string, std::shared_ptr<MockObject>> objectRepo;
+
+    protected:
+        void addObject(std::shared_ptr<MockObject> obj);
+
+        bool hasObjects();
+
+        std::string mainObjectPath;
+        bool started;
+
+    private:
+
+        void registerService();
+
+        std::string name;
+        sdbusplus::bus::bus& bus;
+     
+};
+
+class MockServiceActive : public MockService
+{
+    public:
+        MockServiceActive(std::shared_ptr<sdbusplus::bus::bus> inBus,
+                          std::string serviceName,
+                          SdBusDuration microsecondsToRun,
+                          SdBusDuration timeBetweenSteps = 1s);
+        
+        virtual ~MockServiceActive();
+
+    protected:
+        virtual void run();
+        virtual void proceed();
+
+    private:
+
+        SdBusDuration activeDuration;
+        SdBusDuration timeBetweenSteps;
+        std::thread worker;
+
+};
+
+} // namespace integration
+} // namespace test
+} // namespace sdbusplus

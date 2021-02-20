759951831a868d7f83f94db653e44d9b30886977
Ed Tanous <edtanous@google.com>
2021-02-19 17:14:27 -0800

    Standardize the asio helper methods
    As a general rule, std::networking/asio methods take a single callback
    handler.  This is done intentionally, as it allows objects that cannot
    be taken by reference to be taken directly by move.
    
    The latest helper methods for properties seems to have taken a
    javascript style approach, with "onSuccess" and "onError" handlers.
    This works fine in js, where everything is reference counted, but
    doesn't work in all cases in c++.
    
    As an strawman.
    
    std::unique_ptr<Thing> myThing;
    setProperty(......
               [myThing{std::move(myThing)}](std::error_code){
    	       ... Use MyThing
    	   },
    	   [myThing{std::move(myThing)}](){
    		... Use mything
    	   });
    
    The above code doesn't work, as you can't move out of myThing twice.
    Sure, you could make myThing a shared_ptr, but that's wasteful, and
    defeats c++es policy of "you don't pay for what you don't use".
    
    This commit changes the new handlers to use more standard prototypes
    that accept a single callback, and return error codes on failure.
    
    I was able to find only one usage of these methods in the codebase,
    inside telemetry, with a single getProperty call.  For that method, I
    have left the old version of this method present, and marked it with the
    c++17 [[deprecated]] flag, along with a note on what to do instead.
    This is to avoid a hard binding between this patchset and
    https://gerrit.openbmc-project.xyz/c/openbmc/telemetry/+/40878
    which should allow them to be merged in whatever order we like.
    
    Signed-off-by: Ed Tanous <edtanous@google.com>
    Change-Id: Ia28e41fad5031656cef4d766c59baa948ea781e5

--- include/sdbusplus/asio/property.hpp
+++ include/sdbusplus/asio/property.hpp
@@ -5,38 +5,56 @@
 namespace sdbusplus::asio
 {
 
-template <typename OnError, typename OnSuccess>
+template <typename Handler>
 inline void getAllProperties(sdbusplus::asio::connection& bus,
                              const std::string& service,
                              const std::string& path,
-                             const std::string& interface, OnError&& onError,
-                             OnSuccess&& onSuccess)
+                             const std::string& interface, Handler&& handler)
 {
-    using FunctionTuple = boost::callable_traits::args_t<OnSuccess>;
-    using FunctionTupleType =
-        typename sdbusplus::utility::decay_tuple<FunctionTuple>::type;
-
-    bus.async_method_call(
-        [onError = std::move(onError), onSuccess = std::move(onSuccess)](
-            boost::system::error_code ec,
-            std::tuple_element_t<0, FunctionTupleType>& ret) {
-            if (ec)
-            {
-                onError(ec);
-                return;
-            }
-
-            onSuccess(ret);
-        },
-        service, path, "org.freedesktop.DBus.Properties", "GetAll", interface);
+    bus.async_method_call(std::forward<Handler>(handler), service, path,
+                          "org.freedesktop.DBus.Properties", "GetAll",
+                          interface);
 }
 
-template <typename T, typename OnError, typename OnSuccess>
+template <typename T, typename Handler>
 inline void getProperty(sdbusplus::asio::connection& bus,
                         const std::string& service, const std::string& path,
                         const std::string& interface,
-                        const std::string& propertyName, OnError&& onError,
-                        OnSuccess&& onSuccess)
+                        const std::string& propertyName, Handler&& handler)
+{
+    bus.async_method_call(
+        [handler = std::forward<Handler>(handler)](
+            boost::system::error_code ec,
+            std::variant<std::monostate, T>& ret) {
+            if (ec)
+            {
+                handler(ec, {});
+                return;
+            }
+
+            if (T* value = std::get_if<T>(&ret))
+            {
+                handler(ec, *value);
+                return;
+            }
+
+            handler(boost::system::errc::make_error_code(
+                        boost::system::errc::invalid_argument),
+                    {});
+        },
+        service, path, "org.freedesktop.DBus.Properties", "Get", interface,
+        propertyName);
+}
+
+/* This method has been deprecated, and will be removed in a future revision.
+ * Use the getProperty overload above to make equivalent calls
+ */
+template <typename T, typename OnError, typename OnSuccess>
+[[deprecated]] inline void
+    getProperty(sdbusplus::asio::connection& bus, const std::string& service,
+                const std::string& path, const std::string& interface,
+                const std::string& propertyName, OnError&& onError,
+                OnSuccess&& onSuccess)
 {
     bus.async_method_call(
         [onError = std::move(onError), onSuccess = std::move(onSuccess)](
@@ -61,26 +79,16 @@ inline void getProperty(sdbusplus::asio::connection& bus,
         propertyName);
 }
 
-template <typename T, typename OnError, typename OnSuccess>
+template <typename T, typename Handler>
 inline void setProperty(sdbusplus::asio::connection& bus,
                         const std::string& service, const std::string& path,
                         const std::string& interface,
                         const std::string& propertyName, T&& propertyValue,
-                        OnError&& onError, OnSuccess&& onSuccess)
+                        Handler&& handler)
 {
     bus.async_method_call(
-        [onError = std::move(onError),
-         onSuccess = std::move(onSuccess)](boost::system::error_code ec) {
-            if (ec)
-            {
-                onError(ec);
-                return;
-            }
-
-            onSuccess();
-        },
-        service, path, "org.freedesktop.DBus.Properties", "Set", interface,
-        propertyName,
+        std::forward<Handler>(handler), service, path,
+        "org.freedesktop.DBus.Properties", "Set", interface, propertyName,
         std::variant<std::decay_t<T>>(std::forward<T>(propertyValue)));
 }
 

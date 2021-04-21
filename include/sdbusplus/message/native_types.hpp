52c1490c018aa6f4889d637a1726b9f07d981d84
Ed Tanous <edtanous@google.com>
2021-04-21 08:27:58 -0700

    Fix #60 by adding encoding/decoding rules
    This commit attempts to make encoding more compatible with things within
    openbmc that don't encode paths per the systemd method.  It does this by
    forcing the first character of every path segment to be encoded, so /abc
    would be encoded as /_61bc.  This is then used as a mechanism to
    determine if the path needs to be decoded per systemds rules.
    
    The decode mechanisms are also modified to check whether the first element
    is encoded before decoding the portion.
    
    Looking for input on whether this is an OK path.  The hope would be that
    we slowly transition the system over to using the proper encoding
    mechanisms, with the hope that this patch can be reversed at some point
    in the future.
    
    As an aside, I tried reverting some of the patches, but they're somewhat
    ungainly to revert at this point, and would require reverting some
    bmcweb patches, which, if this patch is determined to be bad, I'm happy
    to do, but I'm hoping this will get us something in the interrim
    
    Tested:
    unit tests added and updated to cover some of the cases.  Unit tests
    pass.
    
    Change-Id: Ie9c8cd0627b8b086e8b0bb2287f28dd86eb98ee9

--- include/sdbusplus/message/native_types.hpp
+++ include/sdbusplus/message/native_types.hpp
@@ -4,6 +4,7 @@
 
 #include <sdbusplus/utility/memory.hpp>
 
+#include <array>
 #include <string>
 
 namespace sdbusplus
@@ -149,6 +150,21 @@ struct string_path_wrapper
     std::string filename() const
     {
         std::string parent = parent_path();
+        size_t firstIndex = parent.size();
+        if (parent != "/")
+        {
+            firstIndex++;
+        }
+        if (firstIndex >= str.size() || parent == "")
+        {
+            return "";
+        }
+        // If we don't see that this was encoded by sdbusplus, return the naive
+        // version of the filename path.
+        if (str[firstIndex] != '_')
+        {
+            return str.substr(firstIndex);
+        }
         _cleanup_free_ char* out = nullptr;
         int r = sd_bus_path_decode(str.c_str(), parent.c_str(), &out);
         if (r <= 0)
@@ -164,7 +180,7 @@ struct string_path_wrapper
         auto index = str.rfind('/');
         if (index == std::string::npos)
         {
-            return string_path_wrapper("/");
+            return string_path_wrapper("");
         }
         if (index <= 1)
         {
@@ -189,6 +205,29 @@ struct string_path_wrapper
             return out;
         }
         out.str = encOut;
+
+        constexpr std::array<char, 16> hex{'0', '1', '2', '3', '4', '5',
+                                           '6', '7', '8', '9', 'a', 'b',
+                                           'c', 'd', 'e', 'f'};
+        if (*extId == '\0')
+        {
+            return out;
+        }
+        size_t firstIndex = str.size();
+        if (str != "/")
+        {
+            firstIndex++;
+        }
+        if (out.str[firstIndex] == '_')
+        {
+            return out;
+        }
+        char firstChar = *extId;
+        out.str.erase(firstIndex, 1);
+        out.str.insert(firstIndex, 1, '_');
+        out.str.insert(firstIndex + 1, 1, hex[firstChar >> 4]);
+        out.str.insert(firstIndex + 2, 1, hex[firstChar & 0xF]);
+
         return out;
     }
 

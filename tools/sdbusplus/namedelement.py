fcff67636ac3bdf58d52606eb1519c5cfbd1a4ef
Patrick Williams <patrick@stwcx.xyz>
2021-04-30 20:18:09 -0500

    sdbus++: add C++20 reserved keywords
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I2cf1e53709d4d4f5572ab676bb9e246e6781d5c4

--- tools/sdbusplus/namedelement.py
+++ tools/sdbusplus/namedelement.py
@@ -30,19 +30,20 @@ class NamedElement(object):
         cppReserved = frozenset({
             "alignas", "alignof", "and", "and_eq", "asm", "auto",
             "bitand", "bitor", "bool", "break", "case", "catch", "char",
-            "char16_t", "char32_t", "class", "compl", "const",
-            "constexpr", "const_cast", "continue", "decltype", "default",
-            "delete", "do", "double", "dynamic_cast", "else", "enum",
-            "explicit", "export", "extern", "false", "float", "for",
+            "char8_t", "char16_t", "char32_t", "class", "compl", "concept",
+            "const", "consteval", "constexpr", "constinit", "const_cast",
+            "continue", "co_await", "co_return", "co_yield", "decltype",
+            "default", "delete", "do", "double", "dynamic_cast", "else",
+            "enum", "explicit", "export", "extern", "false", "float", "for",
             "friend", "goto", "if", "inline", "int", "long", "mutable",
             "namespace", "new", "noexcept", "not", "not_eq", "nullptr",
             "operator", "or", "or_eq", "private", "protected", "public",
-            "register", "reinterpret_cast", "return", "short", "signed",
-            "sizeof", "static", "static_assert", "static_cast", "struct",
-            "switch", "template", "this", "thread_local", "throw", "true",
-            "try", "typedef", "typeid", "typename", "union", "unsigned",
-            "using", "virtual", "void", "volatile", "wchar_t", "while",
-            "xor", "xor_eq"})
+            "register", "reinterpret_cast", "requires", "return", "short",
+            "signed", "sizeof", "static", "static_assert", "static_cast",
+            "struct", "switch", "template", "this", "thread_local", "throw",
+            "true", "try", "typedef", "typeid", "typename", "union",
+            "unsigned", "using", "virtual", "void", "volatile", "wchar_t",
+            "while", "xor", "xor_eq"})
 
         while(name in cppReserved):
             name = name + "_"

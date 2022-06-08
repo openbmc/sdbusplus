openbmc/sdbusplus

commit ce62764d6c0a72ec88eb433e1afd05b751225971

Modified set_handler() to return dbus error code
set_handler() will call the registered callback
if set command is given to a dbus property. The
callback can return error code but the existing
code were not handling it properly.

The following patch replaced sdbusplus exception
with a negative error code in dbus-senors

https://gerrit.openbmc-project.xyz/c/openbmc/dbus-sensors/+/46444

The error code must be used to set the dbus error.
This was handled properly in case of an exception
but not if the callback was returning error code

Signed-off-by: Gokul Sanker V.G <gokul.sanker.v.g@intel.com>
Change-Id: Id164a9b3e62e65ec607d27d2219a3847d2cc7aab

include/sdbusplus/asio/object_server.hpp
@@ -20,6 +20,7 @@
 #include <list>
 #include <optional>
 #include <set>
+#include <tuple>
 
 namespace sdbusplus
 {
@@ -47,7 +48,7 @@ class callback_set
 {
   public:
     virtual ~callback_set() = default;
-    virtual SetPropertyReturnValue call(message_t& m) = 0;
+    virtual std::tuple<SetPropertyReturnValue, int> call(message_t& m) = 0;
     virtual SetPropertyReturnValue set(const boost::any& value) = 0;
 };
 
@@ -311,21 +312,23 @@ class callback_set_instance : public callback_set
         value_(value),
         func_(std::move(func))
     {}
-    SetPropertyReturnValue call(message_t& m) override
+    std::tuple<SetPropertyReturnValue, int> call(message_t& m) override
     {
         PropertyType input;
         m.read(input);
 
         auto oldValue = *value_;
-        if (func_(input, *value_))
+        auto ret = func_(input, *value_);
+
+        if (ret < 0)
         {
-            if (oldValue == *value_)
-            {
-                return SetPropertyReturnValue::sameValueUpdated;
-            }
-            return SetPropertyReturnValue::valueUpdated;
+            return {SetPropertyReturnValue::fail, ret};
         }
-        return SetPropertyReturnValue::fail;
+        if (oldValue == *value_)
+        {
+            return {SetPropertyReturnValue::sameValueUpdated, ret};
+        }
+        return {SetPropertyReturnValue::valueUpdated, ret};
     }
     SetPropertyReturnValue set(const boost::any& value) override
     {
@@ -664,17 +667,26 @@ class dbus_interface
             try
             {
 #endif
-                SetPropertyReturnValue status = func->second->call(mesg);
-                if ((status == SetPropertyReturnValue::valueUpdated) ||
-                    (status == SetPropertyReturnValue::sameValueUpdated))
+                auto [status, rc] = func->second->call(mesg);
+
+                switch (status)
                 {
-                    if (status != SetPropertyReturnValue::sameValueUpdated)
+                    case SetPropertyReturnValue::sameValueUpdated:
+                    {
+                        return true;
+                    }
+                    case SetPropertyReturnValue::valueUpdated:
                     {
                         data->signal_property(property);
+                        return true;
                     }
-                    return true;
+                    case SetPropertyReturnValue::fail:
+                    {
+                        sd_bus_error_set_errno(error, rc);
+                        return false;
+                    }
                 }
-                return false;
+
 #ifdef __EXCEPTIONS
             }
 

commit 8b69ac4cbca27b05d84b4262024a64f25de65046

Clean up ASIO set-property callback code
This reverts commit ce62764d6c0a72ec88eb433e1afd05b751225971, which
introduced inconsistent semantics for the underlying ASIO set handler
functions. The "external" `call` case expected the handler to return an
integer error code, while the "internal" `set` case expected the handler
to return a boolean status.

This also does a small refactor on the two similar `call` and `set`
functions to deduplicate, and adds ASIO.md to document the expected
semantics of the ASIO handlers.

The vast majority of user code in OpenBMC is still returning integers
from the callbacks (0/1/-1), but in most cases 0 was used to mean
failure, and 1 was used to mean success, so formalizing that the
callback should return a boolean value will not break things.

Tested: With accompanying dbus-sensors change
I05c41318954d5d1549752c5ef2c227f3f22a45d3, using steps in that commit
msg.

Signed-off-by: Jonathan Doman <jonathan.doman@intel.com>
Change-Id: Ic3a36027fa95a77469439b296a1497634cfe030f

example/asio-example.cpp
@@ -219,17 +219,17 @@ int server()
                              [](const int& req, int& propertyValue) {
                                  if (req >= 50)
                                  {
-                                     return -EINVAL;
+                                     return false;
                                  }
                                  propertyValue = req;
-                                 return 1; // success
+                                 return true;
                              });
     iface->register_property(
         "TrailTime", std::string("foo"),
         // custom set
         [](const std::string& req, std::string& propertyValue) {
             propertyValue = req;
-            return 1; // success
+            return true;
         },
         // custom get
         [](const std::string& property) {

example/get-all-properties.cpp
@@ -33,7 +33,7 @@ class Application
             propertyGoodbyesName, sdbusplus::vtable::property_::emits_change,
             [this](const auto& newPropertyValue, const auto&) {
                 goodbyes_ = newPropertyValue;
-                return 1;
+                return true;
             },
             [this](const auto&) { return goodbyes_; });
 

example/register-property.cpp
@@ -32,7 +32,7 @@ class Application
                     sdbusplus::vtable::property_::emits_change,
                     [this](const auto& newPropertyValue, const auto&) {
                         goodbyes_ = newPropertyValue;
-                        return 1;
+                        return true;
                     },
                     [this](const auto&) { return goodbyes_; });
             });

include/sdbusplus/asio/object_server.hpp
@@ -20,7 +20,6 @@
 #include <list>
 #include <optional>
 #include <set>
-#include <tuple>
 
 namespace sdbusplus
 {
@@ -48,7 +47,7 @@ class callback_set
 {
   public:
     virtual ~callback_set() = default;
-    virtual std::tuple<SetPropertyReturnValue, int> call(message_t& m) = 0;
+    virtual SetPropertyReturnValue call(message_t& m) = 0;
     virtual SetPropertyReturnValue set(const boost::any& value) = 0;
 };
 
@@ -293,28 +292,22 @@ class callback_set_instance : public callback_set
         value_(value),
         func_(std::move(func))
     {}
-    std::tuple<SetPropertyReturnValue, int> call(message_t& m) override
+    SetPropertyReturnValue call(message_t& m) override
     {
         PropertyType input;
         m.read(input);
-
-        auto oldValue = *value_;
-        auto ret = func_(input, *value_);
-
-        if (ret < 0)
-        {
-            return {SetPropertyReturnValue::fail, ret};
-        }
-        if (oldValue == *value_)
-        {
-            return {SetPropertyReturnValue::sameValueUpdated, ret};
-        }
-        return {SetPropertyReturnValue::valueUpdated, ret};
+        return set_(input);
     }
     SetPropertyReturnValue set(const boost::any& value) override
     {
-        auto oldValue = *value_;
-        if (func_(boost::any_cast<PropertyType>(value), *value_))
+        return set_(boost::any_cast<PropertyType>(value));
+    }
+
+  private:
+    SetPropertyReturnValue set_(const PropertyType& newValue)
+    {
+        PropertyType oldValue = *value_;
+        if (func_(newValue, *value_))
         {
             if (oldValue == *value_)
             {
@@ -467,7 +460,7 @@ class dbus_interface
                 name, property, vtable::property_::emits_change,
                 [](const PropertyType& req, PropertyType& old) {
                     old = req;
-                    return 1;
+                    return true;
                 },
                 [](const PropertyType& value) { return value; });
         }
@@ -638,26 +631,18 @@ class dbus_interface
             try
             {
 #endif
-                auto [status, rc] = func->second->call(mesg);
-
-                switch (status)
+                SetPropertyReturnValue status = func->second->call(mesg);
+                if ((status == SetPropertyReturnValue::valueUpdated) ||
+                    (status == SetPropertyReturnValue::sameValueUpdated))
                 {
-                    case SetPropertyReturnValue::sameValueUpdated:
-                    {
-                        return true;
-                    }
-                    case SetPropertyReturnValue::valueUpdated:
+                    if (status != SetPropertyReturnValue::sameValueUpdated)
                     {
                         data->signal_property(property);
-                        return true;
                     }
-                    case SetPropertyReturnValue::fail:
-                    {
-                        sd_bus_error_set_errno(error, rc);
-                        return false;
-                    }
+                    // There shouldn't be any other callbacks that want to
+                    // handle the message so just return a positive integer.
+                    return 1;
                 }
-
 #ifdef __EXCEPTIONS
             }
 
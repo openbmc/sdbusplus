8fbe8ca5a933e0b2d1d3f5ea44b164555f573e8d
Patrick Williams <patrick@stwcx.xyz>
2022-08-29 09:15:58 -0500

    async: add match
    Define a class which will register for a dbus signal match
    and generate Senders that await new signals.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ib1d77708d74e6063bcaa08fc76fb98667ee7a2cb

--- include/sdbusplus/async.hpp
+++ include/sdbusplus/async.hpp
@@ -2,5 +2,6 @@
 
 #include <sdbusplus/async/context.hpp>
 #include <sdbusplus/async/execution.hpp>
+#include <sdbusplus/async/match.hpp>
 #include <sdbusplus/async/proxy.hpp>
 #include <sdbusplus/async/task.hpp>

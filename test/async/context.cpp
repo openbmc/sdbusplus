49988864fcac43893eab8468f3fa9d9ba5285ad3
Patrick Williams <patrick@stwcx.xyz>
2022-09-20 17:04:17 -0500

    async: add scope
    In order to be able to create multiple subtasks as a collection
    and in order to make sure we keep good track of any tasks that
    are created by the context, add a `scope` as a container of subtasks.
    This is partially modelled after the C++ P2519 proposal.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I6e99b2fa2829d80c320491991f7e038f122963a9

--- test/async/context.cpp
+++ test/async/context.cpp
@@ -8,3 +8,15 @@ TEST(Context, RunSimple)
     ctx.run(std::execution::just() |
             std::execution::then([&ctx]() { ctx.request_stop(); }));
 }
+
+TEST(Context, SpawnedTask)
+{
+    using namespace std::literals;
+
+    sdbusplus::async::context ctx;
+
+    ctx.spawn(std::execution::just());
+
+    ctx.run(std::execution::just() |
+            std::execution::then([&ctx]() { ctx.request_stop(); }));
+}

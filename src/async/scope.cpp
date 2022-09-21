061a3f8d5ec12e507c7f445058eb0d5a6b396bb2
Patrick Williams <patrick@stwcx.xyz>
2022-09-21 08:34:22 -0500

    async: scope: add completion sender
    Add a mechanism to await the completion of an async::scope,
    equivalent to a 'join' on the spawned tasks it contains.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I669680441704cdb59aa982d97bed610178c498b9

--- src/async/scope.cpp
+++ src/async/scope.cpp
@@ -6,7 +6,7 @@ namespace sdbusplus::async
 {
 scope::~scope() noexcept(false)
 {
-    if (count != 0)
+    if (pending_count != 0)
     {
         throw std::logic_error(
             "sdbusplus::async::scope destructed while tasks are pending.");
@@ -15,12 +15,58 @@ scope::~scope() noexcept(false)
 
 void scope::started_task() noexcept
 {
-    ++count;
+    std::lock_guard l{lock};
+    ++pending_count;
 }
 
 void scope::ended_task() noexcept
 {
-    --count;
+    scope_ns::scope_completion* p = nullptr;
+
+    {
+        std::lock_guard l{lock};
+        --pending_count;
+
+        if (pending_count == 0)
+        {
+            p = std::exchange(pending, nullptr);
+        }
+    }
+
+    if (p)
+    {
+        p->complete();
+    }
 }
+
+scope_ns::scope_sender scope::empty() noexcept
+{
+    return scope_ns::scope_sender(*this);
+}
+
+namespace scope_ns
+{
+void scope_completion::arm() noexcept
+{
+    bool done = false;
+
+    {
+        std::lock_guard l{s.lock};
+        if (s.pending_count == 0)
+        {
+            done = true;
+        }
+        else
+        {
+            s.pending = this;
+        }
+    }
+
+    if (done)
+    {
+        this->complete();
+    }
+}
+} // namespace scope_ns
 
 } // namespace sdbusplus::async

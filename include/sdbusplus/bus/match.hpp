afe782be5c66164586e8d57feca4d64205d1dd0b
William A. Kennington III <wak@google.com>
2022-12-09 02:58:54 -0800

    bus/match: Completely constexpr rules
    This uses a constexpr version of to_string to migrate the remaining rule
    functions.
    
    Change-Id: I8a5d8b5c8612c376080ba352a1a28ffec9043586
    Signed-off-by: William A. Kennington III <wak@google.com>

--- include/sdbusplus/bus/match.hpp
+++ include/sdbusplus/bus/match.hpp
@@ -5,6 +5,7 @@
 #include <function2/function2.hpp>
 #include <sdbusplus/bus.hpp>
 #include <sdbusplus/slot.hpp>
+#include <stdplus/numeric/str.hpp>
 #include <stdplus/str/cat.hpp>
 #include <stdplus/zstring.hpp>
 
@@ -106,13 +107,13 @@ constexpr auto destination(std::string_view s) noexcept
 {
     return strCat("destination='"sv, s, "',"sv);
 }
-inline auto argN(size_t n, std::string_view s) noexcept
+constexpr auto argN(size_t n, std::string_view s) noexcept
 {
-    return strCat("arg"sv, std::to_string(n), "='"sv, s, "',"sv);
+    return strCat("arg"sv, stdplus::to_string(n), "='"sv, s, "',"sv);
 }
-inline auto argNpath(size_t n, std::string_view s) noexcept
+constexpr auto argNpath(size_t n, std::string_view s) noexcept
 {
-    return strCat("arg"sv, std::to_string(n), "path='"sv, s, "',"sv);
+    return strCat("arg"sv, stdplus::to_string(n), "path='"sv, s, "',"sv);
 }
 constexpr auto arg0namespace(std::string_view s) noexcept
 {
@@ -148,7 +149,8 @@ constexpr auto interfacesRemoved(std::string_view p) noexcept
     return strCat(interfacesRemoved(), path(p));
 }
 
-inline auto propertiesChanged(std::string_view p, std::string_view i) noexcept
+constexpr auto propertiesChanged(std::string_view p,
+                                 std::string_view i) noexcept
 {
     return strCat(type::signal(), path(p), member("PropertiesChanged"sv),
                   interface("org.freedesktop.DBus.Properties"sv), argN(0, i));
@@ -168,7 +170,7 @@ constexpr auto propertiesChangedNamespace(std::string_view p,
  *
  * @return NameOwnerChanged match string for a service name
  */
-inline auto nameOwnerChanged(std::string_view s) noexcept
+constexpr auto nameOwnerChanged(std::string_view s) noexcept
 {
     return strCat(nameOwnerChanged(), argN(0, s));
 }

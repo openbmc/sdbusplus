3edbe5c0c089b7b5e019de900f997b409de9ac8a
Ed Tanous <edtanous@google.com>
2023-02-28 13:39:53 -0800

    Change io_service to io_context
    This was renamed a while back in boost to be compliant with the
    std::executors proposal.
    
    Change-Id: Ibb55df2b7c1e133621e354728115b89953d9e1bc
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- test/bus/aio.cpp
+++ test/bus/aio.cpp
@@ -1,4 +1,4 @@
-#include <boost/asio/io_service.hpp>
+#include <boost/asio/io_context.hpp>
 #include <sdbusplus/asio/object_server.hpp>
 #include <sdbusplus/bus.hpp>
 
@@ -8,7 +8,7 @@ constexpr auto this_name = "xyz.openbmc_project.sdbusplus.test.Aio";
 
 TEST(AioTest, BasicTest)
 {
-    boost::asio::io_service io;
+    boost::asio::io_context io;
     auto systemBus = std::make_shared<sdbusplus::asio::connection>(io);
     EXPECT_NE(nullptr, systemBus);
 

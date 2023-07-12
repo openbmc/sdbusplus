7cbd35b728a5882c425a67df4d71fbb3609a975a
Ed Tanous <edtanous@google.com>
2023-07-11 18:02:48 -0700

    WIP: allow string_view
    Change-Id: I51c0dde80097835a0efea85ea8f835f44854c63f
    Signed-off-by: Ed Tanous <edtanous@google.com>

--- include/sdbusplus/message/read.hpp
+++ include/sdbusplus/message/read.hpp
@@ -64,6 +64,10 @@ struct can_read_multiple<unix_fd> : std::false_type
 template <>
 struct can_read_multiple<std::string> : std::false_type
 {};
+// std::string_view needs a char* conversion.
+template <>
+struct can_read_multiple<std::string_view> : std::false_type
+{};
 // object_path needs a char* conversion.
 template <>
 struct can_read_multiple<object_path> : std::false_type
@@ -136,7 +140,8 @@ struct read_single
         // For this default implementation, we need to ensure that only
         // basic types are used.
         static_assert(std::is_fundamental_v<Td<T>> ||
-                          std::is_convertible_v<Td<T>, const char*>,
+                          std::is_convertible_v<Td<T>, const char*> ||
+                          std::is_same_v<Td<T>, std::string_view>,
                       "Non-basic types are not allowed.");
 
         constexpr auto dbusType = std::get<0>(types::type_id<T>());
@@ -206,6 +211,25 @@ struct read_single<
             throw exception::SdBusError(-r, "sd_bus_message_read_basic string");
         }
         s = T(str);
+    }
+};
+
+/** @brief Specialization of read_single for std::string_view.
+ */
+template <typename T>
+struct read_single<T, std::enable_if_t<std::is_same_v<T, std::string_view>>>
+{
+    template <typename S>
+    static void op(sdbusplus::SdBusInterface* intf, sd_bus_message* m, S&& s)
+    {
+        constexpr auto dbusType = std::get<0>(types::type_id<S>());
+        const char* str = nullptr;
+        int r = intf->sd_bus_message_read_basic(m, dbusType, &str);
+        if (r < 0)
+        {
+            throw exception::SdBusError(-r, "sd_bus_message_read_basic string");
+        }
+        s = str;
     }
 };
 

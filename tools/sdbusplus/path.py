c0290e4e966e4e725b3004ebd4e99ecc6e079817
Patrick Williams <patrick@stwcx.xyz>
2023-09-01 06:29:45 -0500

    sdbus++: common: generate object_paths
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Iacaa65fa1063b34269d55df3c581e5921dbbc300

---
+++ tools/sdbusplus/path.py
@@ -0,0 +1,44 @@
+import re
+
+from .namedelement import NamedElement
+
+""" Class for parsing 'path' definition elements from an interface.
+"""
+
+
+class Path(NamedElement):
+    def __init__(self, segment=False, **kwargs):
+        if "namespace" in kwargs:
+            kwargs["name"] = "NamespacePath"
+            self.value = kwargs.pop("namespace")
+        elif "instance" in kwargs:
+            kwargs["name"] = "InstancePath"
+            self.value = kwargs.pop("instance")
+        else:
+            self.value = kwargs.pop("value", False)
+
+        # Validate path/segment format.
+        if len(self.value) == 0:
+            raise ValueError("Invalid empty path.")
+        if not segment and self.value[0] != "/":
+            raise ValueError(f"Paths must start with /: {self.value}")
+        if segment and self.value[0] == "/":
+            raise ValueError(f"Segments canot start with /: {self.value}")
+        segments = self.value.split("/")
+        if not segment:
+            segments = segments[1:]
+        for s in segments:
+            if len(s) == 0:
+                raise ValueError(
+                    f"Paths cannot have consecutive /: {self.value} {s}"
+                )
+            if re.search("[^a-zA-Z0-9_]", s):
+                raise ValueError(
+                    f"Path contains illegal characters: {self.value}"
+                )
+
+        self.segments = [
+            Path(segment=True, **s) for s in kwargs.pop("segments", [])
+        ]
+
+        super(Path, self).__init__(**kwargs)

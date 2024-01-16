3e641145179506963548ffd271a4d9361ef267ea
Lei YU <yulei.sh@bytedance.com>
2024-01-16 12:09:08 +0000

    examples: Add match-issue-demo
    The match-issue-demo is to demo an issue related to creating match_t
    with asio resulting in delayed/hang events in asio.
    
    Tested:
    1. Run match_server
    2. Run match_client
    
    After a few seconds, the issue will occur, match_client will be blocked
    due to the match_server does not poll the event in asio:
    ```
     1705407094: Get foo property: foo
     1705407094: Get foo property: foo
     1705407094: Get foo property: foo
    
     # It is blocked here
    ```
    
    The issue is to be fixed in the following commit.
    
    Change-Id: I35aaaf26c241663c0b73b09f8400988df91320f8
    Signed-off-by: Lei YU <yulei.sh@bytedance.com>

---
+++ example/match-issue-demo/match-server.cpp
@@ -0,0 +1,66 @@
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/bus/match.hpp>
+
+#include <chrono>
+
+constexpr auto fooProperty = "foo";
+constexpr auto service = "xyz.openbmc_project.MatchIssueDemo";
+constexpr auto path = "/xyz/openbmc_project/match_issue_demo";
+constexpr auto interface = service;
+
+boost::asio::io_context io;
+auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+
+std::optional<sdbusplus::bus::match_t> match;
+
+void registerMatch()
+{
+    match = sdbusplus::bus::match_t(
+        *conn, sdbusplus::bus::match::rules::propertiesChanged(path, interface),
+        [](sdbusplus::message_t&) {});
+}
+
+void timerHandler()
+{
+    // Create match_t frequently to demo the issue
+    static boost::asio::steady_timer timer(io);
+    timer.expires_after(std::chrono::milliseconds(10));
+    timer.async_wait([](const boost::system::error_code& ec) {
+        if (ec)
+        {
+            return;
+        }
+        registerMatch();
+        timerHandler();
+    });
+}
+
+int main()
+{
+    sdbusplus::asio::object_server objectServer(conn);
+
+    auto fooIfc = objectServer.add_interface(path, interface);
+
+    fooIfc->register_property(
+        "Foo", std::string("foo"),
+        // setter
+        [](const std::string& req, std::string& propertyValue) {
+        propertyValue = req;
+        return 1; // Success
+    },
+        // getter
+        [](const std::string& foo) { return foo; });
+    fooIfc->initialize();
+
+    timerHandler();
+
+    conn->request_name(interface);
+
+    io.run();
+
+    return 0;
+}

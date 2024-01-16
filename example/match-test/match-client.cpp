de99330ba1472ef362e29eb2c26d9e0a0b2abbf9
Lei YU <yulei.sh@bytedance.com>
2024-01-16 12:09:08 +0000

    examples: Add match-test
    The match-test is to demo an issue related to creating match_t with asio
    resulting in delayed events in asio.
    
    Tested:
    1. Run match_server
    2. Run match_client
    
    When the issue occurs, match_client will be blocked due to the
    match_server does not poll the event in asio:
    ```
     1705407094: Get foo property: foo
     1705407094: Get foo property: foo
     1705407094: Get foo property: foo
    
     # It hangs here
    ```
    
    The issue is to be fixed in the following commit.
    
    Change-Id: I35aaaf26c241663c0b73b09f8400988df91320f8
    Signed-off-by: Lei YU <yulei.sh@bytedance.com>

---
+++ example/match-test/match-client.cpp
@@ -0,0 +1,64 @@
+#include <boost/asio.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+#include <sdbusplus/asio/property.hpp>
+#include <sdbusplus/bus/match.hpp>
+
+#include <chrono>
+#include <iostream>
+#include <variant>
+
+std::shared_ptr<sdbusplus::asio::dbus_interface> fooIfc;
+static std::string fooProperty = "foo";
+boost::asio::io_context io;
+auto conn = std::make_shared<sdbusplus::asio::connection>(io);
+
+void syncCall()
+{
+    constexpr auto service = "com.foo";
+    constexpr auto path = "/com/foo";
+    constexpr auto interface = "com.foo";
+    constexpr auto property = "Foo";
+    std::variant<std::string> msg;
+
+    auto& bus = *conn;
+    auto method = bus.new_method_call(service, path,
+                                      "org.freedesktop.DBus.Properties", "Get");
+    method.append(interface, property);
+    auto reply = bus.call(method);
+    reply.read(msg);
+
+    std::string ret = std::get<std::string>(msg);
+    //printf("Get foo property: %s\n", ret.c_str());
+    auto now = std::chrono::system_clock::now();
+    printf("%lu: Get foo property: %s\n",
+           std::chrono::system_clock::to_time_t(now),
+           ret.c_str());
+    // if fail, throw exception, don't catch, will exit
+}
+
+void syncTimer()
+{
+    static boost::asio::steady_timer timer(io);
+    timer.expires_after(std::chrono::milliseconds(10));
+    timer.async_wait([](const boost::system::error_code& ec) {
+        if (ec)
+        {
+            return;
+        }
+        syncCall();
+        syncTimer();
+    });
+}
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    syncTimer();
+
+    io.run();
+
+    return 0;
+}

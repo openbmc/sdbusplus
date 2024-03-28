bc74fddf877800a0b7910aef8582a7e19c816bfb
Ed Tanous <ed@tanous.net>
2024-03-27 17:56:21 -0700

    WIP: Use sd-bus array methods for read and append
    Currently sdbusplus uses sd_bus_message_read_basic and
    message_append_basic for decoding and encoding of arrays.  For large
    arrays, this imposes a significant overhead in calling the library each
    time.  This leads to some extreme performance degradation in some cases,
    for example, passing a 4K bytes data array over sdbusplus interface now
    takes 4096 + 2 calls of sd_bus_message_read_basic and
    message_append_basic, this will consume about 10ms CPU time on a Aspeed
    2600 platform for each package on both send and receive side.
    
    While in this case, a DBus interface design should likely opt for using
    an FD rather than an array of bytes, this isn't a reason to not optimize
    this case.
    
    sd-bus, in version 240 added methods to deal with this performance
    degradation, namely sd_bus_message_read_array and
    sd_bus_message_append_array, which each only require a single call to
    load values into the array using pointers and lengths.
    
    This patchset is based on the one submitted here:
    https://gerrit.openbmc.org/c/openbmc/sdbusplus/+/68614
    
    But opts for a different approach to a number of technical details on
    how it accomplishes the result, including changing the underlying sd-bus
    calls used to utilize ones that don't require sdbus internal malloc,
    avoiding unit tests ASAN issues in the process.
    
    This commit adds support for utilizing the above methods when doing
    calls to the library if the following requirements are met:
    1. The container is contiguous (using the std::contiguous_iterator
    concept)
    2. The container represents a list of values of type int/uint 8,16,32,
    or 64 bits wide.  Note, per the sd-bus documentation, arrays of bool are
    explicitly not supported (presumably because the internal representation
    of a bool array for both libc++ and sd-bus is implementation defined).
    
    To accomplish this, the arrays handling for contiguous arrays is moved
    to use concepts, simplifying the code significantly, and allowing the
    use of std::contiguous_iterator.
    
    Change-Id: I3bd9f97ed160835e8c30c302b80553a1d450bbf9
    Signed-off-by: Ed Tanous <ed@tanous.net>
    Signed-off-by: Yongbing Chen <yongbingchen@google.com>

--- include/sdbusplus/utility/container_traits.hpp
+++ include/sdbusplus/utility/container_traits.hpp
@@ -5,94 +5,31 @@
 
 namespace sdbusplus
 {
-namespace utility
-{
-
-/** has_const_iterator - Determine if type has const iterator
- *
- *  @tparam T - Type to be tested.
- *
- *  @value A value as to whether or not the type supports iteration
- */
-template <typename T>
-struct has_const_iterator
-{
-  private:
-    typedef char yes;
-    typedef struct
-    {
-        char array[2];
-    } no;
-
-    template <typename C>
-    static constexpr yes test(decltype(std::cbegin(std::declval<C>()))*);
-    template <typename C>
-    static constexpr no test(...);
-
-  public:
-    static constexpr bool value = sizeof(test<T>(nullptr)) == sizeof(yes);
-};
 
 template <typename T>
-inline constexpr bool has_const_iterator_v = has_const_iterator<T>::value;
+concept IsDbusArray = requires(T v) {
+                          { v.begin() } -> std::forward_iterator;
+                          !std::is_same_v<std::decay<T>, std::string>;
+                      };
 
-/** has_emplace_method - Determine if type has a method template named emplace
- *
- *  @tparam T - Type to be tested.
- *
- *  @value A value as to whether or not the type has an emplace method
- */
-template <typename T>
-struct has_emplace_method
-{
-  private:
-    struct dummy
-    {};
-
-    template <typename C, typename P>
-    static constexpr auto test(P* p)
-        -> decltype(std::declval<C>().emplace(*p), std::true_type());
-
-    template <typename, typename>
-    static std::false_type test(...);
-
-  public:
-    static constexpr bool value =
-        std::is_same_v<std::true_type, decltype(test<T, dummy>(nullptr))>;
-};
+template <typename T, typename... U>
+concept IsAnyOf = (std::same_as<T, U> || ...);
 
 template <typename T>
-inline constexpr bool has_emplace_method_v = has_emplace_method<T>::value;
-
-/** has_emplace_method - Determine if type has a method template named
- * emplace_back
- *
- *  @tparam T - Type to be tested.
- *
- *  @value A value as to whether or not the type has an emplace_back method
- */
-template <typename T>
-struct has_emplace_back_method
-{
-  private:
-    struct dummy
-    {};
-
-    template <typename C, typename P>
-    static constexpr auto test(P* p)
-        -> decltype(std::declval<C>().emplace_back(*p), std::true_type());
-
-    template <typename, typename>
-    static std::false_type test(...);
-
-  public:
-    static constexpr bool value =
-        std::is_same_v<std::true_type, decltype(test<T, dummy>(nullptr))>;
-};
+concept CanAppendArrayValue =
+    requires(T v) {
+        // Require that the container be contiguous
+        { v.begin() } -> std::contiguous_iterator;
+        // The sd-bus append array docs are very clear about what types are
+        // allowed; verify this is one of those types.
+        requires(IsAnyOf<typename T::value_type, uint8_t, int16_t, uint16_t,
+                         int32_t, uint32_t, int64_t, uint64_t, double>);
+    };
 
 template <typename T>
-inline constexpr bool has_emplace_back_method_v =
-    has_emplace_back_method<T>::value;
+concept HasEmplaceBack = requires(T v) { v.emplace_back(); };
 
-} // namespace utility
+template <typename T>
+concept HasEmplace = requires(T v) { v.emplace(); };
+
 } // namespace sdbusplus

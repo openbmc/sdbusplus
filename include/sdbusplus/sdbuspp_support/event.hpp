522d53a9de13b1c9d085261bfb0f26f00cdffbd8
Patrick Williams <patrick@stwcx.xyz>
2024-11-05 21:35:55 -0500

    sdbus++: events: unpack an event from JSON
    Generate code necessary to unpack a generated event from its
    JSON representation.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I7a28617f3f0374968609328a44587b655a9141c1

---
+++ include/sdbusplus/sdbuspp_support/event.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <nlohmann/json.hpp>
+#include <sdbusplus/exception.hpp>
+
+#include <source_location>
+
+namespace sdbusplus::sdbuspp
+{
+
+using register_hook =
+    std::function<void(const nlohmann::json&, const std::source_location&)>;
+
+void register_event(const std::string&, register_hook);
+
+template <typename T>
+struct register_event_helper
+{
+    static void hook()
+    {
+        register_event(T::errName, throw_event);
+    }
+
+    static void throw_event(const nlohmann::json& j,
+                            const std::source_location& location)
+    {
+        throw T(j, location);
+    }
+};
+} // namespace sdbusplus::sdbuspp

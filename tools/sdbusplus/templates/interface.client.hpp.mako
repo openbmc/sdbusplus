openbmc/sdbusplus

commit 8aea1d814330427a3668b48c347f1703c214748e

sdbus++: generate `properties` method for client harness
This uses the existing proxy `get_all_properties` method and allows for
more efficient fetching of all properties in one shot. A struct is
generated for each type, and each field is initialized with its default
value (but is not guaranteed to be deserialized).

As errors are less detectable (e.g. perhaps an expected property
did not come through?), I considered whether the values in the struct
should all be `std::optional`. However, given how other methods are
used, it feels like this would result in a crash and assertion
failure rather than saving anyone time and effort. The wrong type
is detected a suitable exception is thrown in those cases.

Change-Id: Iff7712bd17db39f1ee5699f867e9f17a54eea21b
Signed-off-by: Adin Scannell <adin@scannell.ca>

tools/sdbusplus/templates/interface.client.hpp.mako
@@ -1,6 +1,8 @@
 #pragma once
 #include <sdbusplus/async/client.hpp>
+#include <sdbusplus/async/execution.hpp>
 #include <type_traits>
+#include <variant>
 
 % for h in interface.cpp_includes():
 #include <${h}>
@@ -47,6 +49,13 @@ class ${interface.classname} :
     private sdbusplus::async::client::details::client_context_friend
 {
   public:
+    struct properties_t
+    {
+        % for p in interface.properties:
+        ${p.cppTypeParam(interface.name)} ${p.snake_case}${p.default_value(interface.name)};
+        % endfor
+    };
+
     friend Client;
     template <typename, typename>
     friend struct sdbusplus::client::${interface.cppNamespacedClass()};
@@ -61,6 +70,41 @@ ${m.render(loader, "method.client.hpp.mako", method=m, interface=interface)}
     % for p in interface.properties:
 ${p.render(loader, "property.client.hpp.mako", property=p, interface=interface)}
     % endfor
+
+    auto properties()
+    {
+        return proxy.template get_all_properties<PropertiesVariant>(context()) |
+               sdbusplus::async::execution::then([](auto&& v) {
+                   properties_t result;
+                   for (const auto& [property, value] : v)
+                   {
+                       std::visit(
+                           [&](auto v) {
+                               % for p in interface.properties:
+                               if (property == "${p.name}")
+                               {
+                                   if constexpr (std::is_same_v<
+                                                     std::decay_t<decltype(v)>,
+                                                     ${p.cppTypeParam(interface.name)}>)
+                                   {
+                                       result.${p.snake_case} = v;
+                                       return;
+                                   }
+                                   else
+                                   {
+                                       throw exception::UnpackPropertyError(
+                                           property,
+                                           UnpackErrorReason::wrongType);
+                                   }
+                               }
+                               % endfor
+                           },
+                           value);
+                   }
+                   return result;
+               });
+    }
+
   private:
     // Conversion constructor from proxy used by client_t.
     explicit constexpr ${interface.classname}(Proxy p) :

tools/sdbusplus/templates/interface.common.hpp.mako
@@ -34,6 +34,8 @@ struct ${interface.classname}
     % if interface.properties:
     using PropertiesVariant = sdbusplus::utility::dedup_variant_t<
         ${",\n        ".join(sorted(setOfPropertyTypes()))}>;
+    % else:
+    using PropertiesVariant = std::variant<std::monostate>;
     % endif \
 
     % for p in interface.paths:

example/calculator-client.cpp
@@ -59,6 +59,13 @@ auto startup(sdbusplus::async::context& ctx) -> sdbusplus::async::task<>
         std::cout << "Should be 'client': " << _ << std::endl;
     }
 
+    {
+        // Grab all the properties and print them.
+        auto _ = co_await c.properties();
+        std::cout << "Should be 1234: " << _.last_result << std::endl;
+        std::cout << "Should be 'client': " << _.owner << std::endl;
+    }
+
     co_return;
 }
 

tools/meson.build
@@ -43,7 +43,6 @@ sdbusplusplus_depfiles = files(
   'sdbusplus/templates/property.aserver.set.hpp.mako',
   'sdbusplus/templates/property.aserver.tag.hpp.mako',
   'sdbusplus/templates/property.aserver.typeid.hpp.mako',
-  'sdbusplus/templates/property.aserver.value.hpp.mako',
   'sdbusplus/templates/property.aserver.vtable.hpp.mako',
   'sdbusplus/templates/property.client.hpp.mako',
   'sdbusplus/templates/property.server.cpp.mako',

tools/sdbusplus/property.py
@@ -86,11 +86,15 @@ class Property(NamedElement, Renderer):
     def cppTypeParam(self, interface, full=False, typename="common"):
         return self.__cppTypeParam(interface, self.cppTypeName, full, typename)
 
-    def default_value(self):
-        if self.defaultValue is not None:
-            return " = " + str(self.defaultValue)
-        else:
+    def default_value(self, interface):
+        if self.defaultValue is None:
             return ""
+        value = str(self.defaultValue)
+        enum_prefix = ""
+        if self.is_enum():
+            p_type = self.cppTypeParam(interface)
+            enum_prefix = f"{p_type}::"
+        return f" = {enum_prefix}{value}"
 
     def __cppTypeParam(
         self, interface, cppTypeName, full=False, typename="common"

tools/sdbusplus/templates/interface.aserver.hpp.mako
@@ -87,7 +87,7 @@ ${p.render(loader, "property.aserver.set.hpp.mako", property=p, interface=interf
 
   protected:
 % for p in interface.properties:
-${p.render(loader, "property.aserver.value.hpp.mako", property=p, interface=interface)}\
+    ${p.cppTypeParam(interface.name)} ${p.snake_case}_${p.default_value(interface.name)};
 % endfor
 
   private:

tools/sdbusplus/templates/property.aserver.value.hpp.mako
@@ -1,17 +1,0 @@
-<%
-p_name = property.snake_case
-p_type = property.cppTypeParam(interface.name)
-
-def p_value():
-    if property.defaultValue == None:
-        return "{}"
-
-    value = str(property.defaultValue)
-    enum_prefix = ""
-    if property.is_enum():
-        enum_prefix = f"{p_type}::"
-
-    return f" = {enum_prefix}{value}"
-
-%>\
-    ${p_type} ${p_name}_${p_value()};

tools/sdbusplus/templates/signal.aserver.emit.hpp.mako
@@ -5,17 +5,12 @@
 
     def parameter(p):
         r = "%s %s%s" % \
-            (p.cppTypeParam(interface.name), p.camelCase, default_value(p))
+            (p.cppTypeParam(interface.name), p.camelCase, p.default_value(interface.name))
         return r
 
     def parameters_as_list():
         return ", ".join([ p.camelCase for p in signal.properties ])
 
-    def default_value(p):
-        if p.defaultValue != None:
-            return " = " + str(p.defaultValue)
-        else:
-            return ""
 %>\
     /** @brief Send signal '${signal.name}'
      *

commit 2dd0380b9202993aa5ce2e49a1667be2b9b3e5ae

sdbus++: remove PropertiesVariant for empty properties
A previous commit changed behavior to always add PropertiesVariant
even when there were no properties (and to use `variant<monostate>`).
The missing PropertiesVariant was used by some other repositories to
detect if an interface had properties or not.  Adding an empty
PropertiesVariant breaks this expectation.

Switch back to having no type defined when there are no properties.

Fixes: 8aea1d814330 ("sdbus++: generate `properties` method for client harness")
Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: I75b36412fe42eb44d5a638c6a8ad7a3ee9b835e3

tools/sdbusplus/templates/interface.client.hpp.mako
@@ -71,6 +71,7 @@ ${m.render(loader, "method.client.hpp.mako", method=m, interface=interface)}
 ${p.render(loader, "property.client.hpp.mako", property=p, interface=interface)}
     % endfor
 
+    % if interface.properties:
     auto properties()
     {
         return proxy.template get_all_properties<PropertiesVariant>(context()) |
@@ -104,6 +105,7 @@ ${p.render(loader, "property.client.hpp.mako", property=p, interface=interface)}
                    return result;
                });
     }
+    % endif
 
   private:
     // Conversion constructor from proxy used by client_t.

tools/sdbusplus/templates/interface.common.hpp.mako
@@ -34,8 +34,6 @@ struct ${interface.classname}
     % if interface.properties:
     using PropertiesVariant = sdbusplus::utility::dedup_variant_t<
         ${",\n        ".join(sorted(setOfPropertyTypes()))}>;
-    % else:
-    using PropertiesVariant = std::variant<std::monostate>;
     % endif \
 
     % for p in interface.paths:
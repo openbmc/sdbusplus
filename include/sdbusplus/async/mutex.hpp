35511713a8ecaff707b279399cbf06bf9b1d52f8
Jagpal Singh Gill <paligill@gmail.com>
2025-07-07 16:34:20 -0700

    add support for async mutex
    Add support for async mutex for synchronizing coroutines/tasks using
    sender receiver framework. Also, add lock_guard support which provides a
    RAII wrapper for mutex to own it for the duration of a scoped block.
    
    Tested: Add Unit Test using gtest
    ```
    1/1 test_async_mutex OK              2.01s
    
    Ok:                 1
    Expected Fail:      0
    Fail:               0
    Unexpected Pass:    0
    Skipped:            0
    Timeout:            0
    ```
    
    Change-Id: I691528885a94b9cf55d4b7f2fb0c1e0e6d0ab84f
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

---
+++ include/sdbusplus/async/mutex.hpp
@@ -0,0 +1,153 @@
+#pragma once
+
+#include <sdbusplus/async/execution.hpp>
+#include <sdbusplus/event.hpp>
+
+#include <atomic>
+#include <queue>
+#include <string>
+
+namespace sdbusplus::async
+{
+
+namespace mutex_ns
+{
+struct mutex_completion;
+} // namespace mutex_ns
+
+class lock_guard;
+
+class mutex
+{
+  public:
+    mutex() = delete;
+    mutex(const mutex&) = delete;
+    mutex& operator=(const mutex&) = delete;
+    mutex(mutex&&) = delete;
+    mutex& operator=(mutex&&) = delete;
+    ~mutex() = default;
+
+    mutex(const std::string& name = "sdbusplus::async::mutex");
+
+    friend mutex_ns::mutex_completion;
+    friend lock_guard;
+
+  private:
+    void unlock();
+
+    std::string name;
+    std::atomic<bool> locked{false};
+    std::queue<mutex_ns::mutex_completion*> waitingTasks;
+    std::mutex lock{};
+};
+
+// RAII wrapper for mutex for the duration of a scoped block.
+class lock_guard
+{
+  public:
+    lock_guard() = delete;
+    lock_guard(const lock_guard&) = delete;
+    lock_guard& operator=(const lock_guard&) = delete;
+    lock_guard(lock_guard&&) = delete;
+    lock_guard& operator=(lock_guard&&) = delete;
+
+    explicit lock_guard(mutex& mutexInstance) : mutexInstance(mutexInstance) {}
+
+    ~lock_guard()
+    {
+        mutexInstance.unlock();
+    }
+
+    auto lock() noexcept;
+
+  private:
+    mutex& mutexInstance;
+};
+
+namespace mutex_ns
+{
+
+struct mutex_completion
+{
+    mutex_completion() = delete;
+    mutex_completion(const mutex_completion&) = delete;
+    mutex_completion& operator=(const mutex_completion&) = delete;
+    mutex_completion(mutex_completion&&) = delete;
+    ~mutex_completion() = default;
+
+    explicit mutex_completion(mutex& mutexInstance) noexcept :
+        mutexInstance(mutexInstance) {};
+
+    friend mutex;
+
+    friend void tag_invoke(execution::start_t, mutex_completion& self) noexcept
+    {
+        self.arm();
+    }
+
+  protected:
+    bool tryLock();
+
+  private:
+    virtual void complete() noexcept = 0;
+    virtual void stop() noexcept = 0;
+    void arm() noexcept;
+
+    mutex& mutexInstance;
+};
+
+// Implementation (templated based on Receiver) of mutex_completion.
+template <execution::receiver Receiver>
+struct mutex_operation : mutex_completion
+{
+    mutex_operation(mutex& mutexInstance, Receiver r) :
+        mutex_completion(mutexInstance), receiver(std::move(r))
+    {}
+
+  private:
+    void complete() noexcept override final
+    {
+        execution::set_value(std::move(receiver));
+    }
+
+    void stop() noexcept override final
+    {
+        execution::set_stopped(std::move(receiver));
+    }
+
+    Receiver receiver;
+};
+
+// mutex sender
+struct mutex_sender
+{
+    using is_sender = void;
+
+    mutex_sender() = delete;
+    explicit mutex_sender(mutex& mutexInstance) noexcept :
+        mutexInstance(mutexInstance) {};
+
+    friend auto tag_invoke(execution::get_completion_signatures_t,
+                           const mutex_sender&, auto)
+        -> execution::completion_signatures<execution::set_value_t(),
+                                            execution::set_stopped_t()>;
+
+    template <execution::receiver R>
+    friend auto tag_invoke(execution::connect_t, mutex_sender&& self, R r)
+        -> mutex_operation<R>
+    {
+        return {self.mutexInstance, std::move(r)};
+    }
+
+  private:
+    mutex& mutexInstance;
+};
+
+} // namespace mutex_ns
+
+inline auto lock_guard::lock() noexcept
+{
+    return mutex_ns::mutex_sender{this->mutexInstance};
+}
+
+} // namespace sdbusplus::async
---
+++ test/async/mutex.cpp
@@ -0,0 +1,145 @@
+#include <sdbusplus/async.hpp>
+
+#include <filesystem>
+#include <fstream>
+
+#include <gtest/gtest.h>
+
+using namespace std::literals;
+
+namespace fs = std::filesystem;
+
+class MutexTest : public ::testing::Test
+{
+  protected:
+    constexpr static std::string testMutex = "TestMutex";
+    const fs::path path = "/tmp";
+
+    MutexTest() : mutex(testMutex)
+    {
+        auto fd = inotify_init1(IN_NONBLOCK);
+        EXPECT_NE(fd, -1) << "Error occurred during the inotify_init1, error: "
+                          << errno;
+
+        auto wd = inotify_add_watch(fd, path.c_str(), IN_CLOSE_WRITE);
+        EXPECT_NE(wd, -1)
+            << "Error occurred during the inotify_add_watch, error: " << errno;
+        fdioInstance = std::make_unique<sdbusplus::async::fdio>(*ctx, fd);
+    }
+
+    std::optional<sdbusplus::async::context> ctx{std::in_place};
+
+    auto testAsyncAddition(int val = 1) -> sdbusplus::async::task<>
+    {
+        sdbusplus::async::lock_guard lg{mutex};
+        co_await lg.lock();
+
+        sharedVar += val;
+    }
+
+    auto testAsyncSubtraction(int val = 1) -> sdbusplus::async::task<>
+    {
+        sdbusplus::async::lock_guard lg{mutex};
+        co_await lg.lock();
+
+        sharedVar -= val;
+    }
+
+    auto writeToFile() -> sdbusplus::async::task<>
+    {
+        std::ofstream outfile((path / testMutex).native());
+        EXPECT_TRUE(outfile.is_open())
+            << "Error occurred during file open, error: " << errno;
+
+        outfile << testMutex << std::endl;
+        outfile.close();
+
+        co_return;
+    }
+
+    auto readFromFile() -> sdbusplus::async::task<>
+    {
+        std::ifstream infile((path / testMutex).native());
+        EXPECT_TRUE(infile.is_open())
+            << "Error occurred during file open, error: " << errno;
+
+        std::string line;
+        std::getline(infile, line);
+        EXPECT_EQ(line, testMutex);
+        infile.close();
+
+        co_return;
+    }
+
+    auto testFdEvents() -> sdbusplus::async::task<>
+    {
+        sdbusplus::async::lock_guard lg{mutex};
+        co_await lg.lock();
+
+        ctx->spawn(writeToFile());
+        co_await fdioInstance->next();
+        co_await readFromFile();
+        ran++;
+
+        co_return;
+    }
+
+    int sharedVar = 0;
+    int ran = 0;
+
+  private:
+    sdbusplus::async::mutex mutex;
+    std::unique_ptr<sdbusplus::async::fdio> fdioInstance;
+    int fd = -1;
+    int wd = -1;
+};
+
+TEST_F(MutexTest, TestAsyncAddition)
+{
+    constexpr auto testIterations = 10;
+    for (auto i = 0; i < testIterations; i++)
+    {
+        ctx->spawn(testAsyncAddition());
+    }
+
+    ctx->spawn(
+        sdbusplus::async::sleep_for(*ctx, 1s) |
+        sdbusplus::async::execution::then([&]() { ctx->request_stop(); }));
+
+    ctx->run();
+
+    EXPECT_EQ(sharedVar, testIterations);
+}
+
+TEST_F(MutexTest, TestAsyncMixed)
+{
+    constexpr auto testIterations = 10;
+    for (auto i = 0; i < testIterations; i++)
+    {
+        ctx->spawn(testAsyncAddition());
+        ctx->spawn(testAsyncSubtraction(2));
+    }
+
+    ctx->spawn(
+        sdbusplus::async::sleep_for(*ctx, 1s) |
+        sdbusplus::async::execution::then([&]() { ctx->request_stop(); }));
+
+    ctx->run();
+
+    EXPECT_EQ(sharedVar, -testIterations);
+}
+
+TEST_F(MutexTest, TestFdEvents)
+{
+    constexpr static auto testIterations = 5;
+
+    for (auto i = 0; i < testIterations; i++)
+    {
+        ctx->spawn(testFdEvents());
+    }
+    ctx->spawn(
+        sdbusplus::async::sleep_for(*ctx, 3s) |
+        sdbusplus::async::execution::then([&]() { ctx->request_stop(); }));
+    ctx->run();
+    EXPECT_EQ(ran, testIterations);
+}

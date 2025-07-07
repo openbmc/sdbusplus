5e61e6e3933af5e59ee219a631dc9b7dd634fe30
Jagpal Singh Gill <paligill@gmail.com>
2025-07-07 16:34:20 -0700

    add support for async mutex
    Add support for async mutex for synchronizing coroutines/tasks using
    sender receiver framework. Also, add lock_guard support which provides a
    RAII wrapper for mutex to own it for the duration of a scoped block.
    
    Tested: Add Unit Test using gtest
    ```
    1/1 test_async_mutex OK              2.01s
    
    Ok:                 1
    Expected Fail:      0
    Fail:               0
    Unexpected Pass:    0
    Skipped:            0
    Timeout:            0
    ```
    
    Change-Id: I691528885a94b9cf55d4b7f2fb0c1e0e6d0ab84f
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- include/sdbusplus/async.hpp
+++ include/sdbusplus/async.hpp
@@ -5,6 +5,7 @@
 #include <sdbusplus/async/execution.hpp>
 #include <sdbusplus/async/fdio.hpp>
 #include <sdbusplus/async/match.hpp>
+#include <sdbusplus/async/mutex.hpp>
 #include <sdbusplus/async/proxy.hpp>
 #include <sdbusplus/async/task.hpp>
 #include <sdbusplus/async/timer.hpp>

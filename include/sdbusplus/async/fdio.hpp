8d88ab16c877f9462df0d2852cedad9cc885f7ae
Jagpal Singh Gill <paligill@gmail.com>
2025-07-22 18:25:36 -0700

    add support for timed fdio
    For certain operations such as reading and writing to a fd for request
    response flows, the fd needs to have a timeout to avoid everlasting
    hangs in case of a unresponsive responder. To support this, the fdio has
    been extended to add timeout support via new API timed_next().
    
    Tested: Unit Test
    ```
    1/1 test_async_fdio_timed OK             10.03s
    
    Ok:                 1
    Expected Fail:      0
    Fail:               0
    Unexpected Pass:    0
    Skipped:            0
    Timeout:            0
    ```
    
    Change-Id: Ib219e4a4c55125785e6c1571488e445ba3379244
    Signed-off-by: Jagpal Singh Gill <paligill@gmail.com>

--- include/sdbusplus/async/fdio.hpp
+++ include/sdbusplus/async/fdio.hpp
@@ -22,8 +22,8 @@ class fdio : private context_ref, details::context_friend
     fdio& operator=(fdio&&) = delete;
     ~fdio() = default;
 
-    /** Construct a new fdio object from a context and a file descriptor. */
-    fdio(context& ctx, int fd);
+    fdio(context& ctx, int fd,
+         std::chrono::microseconds timeout = std::chrono::microseconds(0));
 
     /** Get the next fd event.
      *  Note: the implementation only supports a single awaiting task.  Two
@@ -31,9 +31,16 @@ class fdio : private context_ref, details::context_friend
      */
     auto next() noexcept;
 
+    /** Get the next fd event with a timeout.
+     *  Note: the implementation only supports a single awaiting task.  Two
+     *  tasks should not share this object and both call `timed_next()`.
+     */
+    auto timed_next() noexcept;
+
     friend fdio_ns::fdio_completion;
 
   private:
+    event_t::time_resolution timeout;
     event_source_t source;
     std::mutex lock{};
     fdio_ns::fdio_completion* complete{nullptr};
@@ -44,6 +51,7 @@ class fdio : private context_ref, details::context_friend
     }
 
     void handleEvent() noexcept;
+    void handleTimeout() noexcept;
 };
 
 namespace fdio_ns
@@ -68,11 +76,13 @@ struct fdio_completion
     }
 
   private:
-    virtual void complete() noexcept = 0;
+    virtual void complete(bool timedOut) noexcept = 0;
+    virtual void error(std::exception_ptr exceptionPtr) noexcept = 0;
     virtual void stop() noexcept = 0;
     void arm() noexcept;
 
     fdio& fdioInstance;
+    event_source_t source;
 };
 
 // Implementation (templated based on Receiver) of fdio_completion.
@@ -84,10 +94,12 @@ struct fdio_operation : fdio_completion
     {}
 
   private:
-    void complete() noexcept override final
+    void complete(bool) noexcept override final
     {
         execution::set_value(std::move(receiver));
     }
+
+    void error(std::exception_ptr) noexcept override final {}
 
     void stop() noexcept override final
     {
@@ -122,11 +134,77 @@ struct fdio_sender
     fdio& fdioInstance;
 };
 
+// Implementation (templated based on Receiver) of fdio_completion for timed
+// operations.
+template <execution::receiver Receiver>
+struct fdio_timed_operation : fdio_completion
+{
+    fdio_timed_operation(fdio& fdioInstance, Receiver r) :
+        fdio_completion(fdioInstance), receiver(std::move(r))
+    {}
+
+  private:
+    void complete(bool timedOut) noexcept override final
+    {
+        if (timedOut)
+        {
+            execution::set_value(std::move(receiver), false);
+            return;
+        }
+
+        execution::set_value(std::move(receiver), true);
+    }
+
+    void error(std::exception_ptr exceptionPtr) noexcept override final
+    {
+        execution::set_error(std::move(receiver), exceptionPtr);
+    }
+
+    void stop() noexcept override final
+    {
+        execution::set_stopped(std::move(receiver));
+    }
+
+    Receiver receiver;
+};
+
+// fdio timed Sender implementation.
+struct fdio_timed_sender
+{
+    using is_sender = void;
+
+    fdio_timed_sender() = delete;
+    explicit fdio_timed_sender(fdio& fdioInstance) noexcept :
+        fdioInstance(fdioInstance) {};
+
+    friend auto tag_invoke(execution::get_completion_signatures_t,
+                           const fdio_timed_sender&, auto)
+        -> execution::completion_signatures<
+            execution::set_value_t(bool),
+            execution::set_error_t(std::exception_ptr),
+            execution::set_stopped_t()>;
+
+    template <execution::receiver R>
+    friend auto tag_invoke(execution::connect_t, fdio_timed_sender&& self, R r)
+        -> fdio_timed_operation<R>
+    {
+        return {self.fdioInstance, std::move(r)};
+    }
+
+  private:
+    fdio& fdioInstance;
+};
+
 } // namespace fdio_ns
 
 inline auto fdio::next() noexcept
 {
     return fdio_ns::fdio_sender{*this};
+}
+
+inline auto fdio::timed_next() noexcept
+{
+    return fdio_ns::fdio_timed_sender{*this};
 }
 
 } // namespace sdbusplus::async

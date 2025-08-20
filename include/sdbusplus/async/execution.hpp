afc8e6e1934ce9e26f9239c634e0e3e1d5772e41
Patrick Williams <patrick@stwcx.xyz>
2025-08-20 12:37:23 -0400

    use external stdexec package
    Add subprojects/stdexec.wrap so that we can use upstream stdexec,
    if the packageconfig is unavailable, and reference it as necessary
    in the meson.build.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ic0f32f694aac2f98c9646168b08ca352c183afa1

--- include/sdbusplus/async/execution.hpp
+++ include/sdbusplus/async/execution.hpp
@@ -1,27 +1,11 @@
 #pragma once
 
-// The upstream code has some warnings under GCC, so turn them off
-// as needed.
-
-#pragma GCC diagnostic push
-#ifdef __clang__
-#pragma clang diagnostic ignored "-Wunused-parameter"
-#pragma clang diagnostic ignored "-Wnon-pod-varargs"
-#pragma clang diagnostic ignored "-Winconsistent-missing-override"
-#pragma clang diagnostic ignored "-Wdeprecated-copy"
-#else
-#pragma GCC diagnostic ignored "-Wnon-template-friend"
-#endif
-#pragma GCC diagnostic ignored "-Wempty-body"
-#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <sdbusplus/async/stdexec/async_scope.hpp>
-#include <sdbusplus/async/stdexec/coroutine.hpp>
-#include <sdbusplus/async/stdexec/execution.hpp>
-#pragma GCC diagnostic pop
+#include <exec/async_scope.hpp>
+#include <stdexec/coroutine.hpp>
+#include <stdexec/execution.hpp>
 
 // Add std::execution as sdbusplus::async::execution so that we can simplify
-// reference to any parts of it we use internally.
+// reference to any parts of it we use.
 namespace sdbusplus::async
 {
 namespace execution = stdexec;

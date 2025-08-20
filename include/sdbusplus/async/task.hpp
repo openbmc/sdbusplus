a82ce4076e35010acd491912709aa1a3d0fd7f5d
Patrick Williams <patrick@stwcx.xyz>
2025-08-20 12:37:23 -0400

    use external stdexec package
    Add subprojects/stdexec.wrap so that we can use upstream stdexec,
    if the packageconfig is unavaiable, and reference it as necessary
    in the meson.build.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: Ic0f32f694aac2f98c9646168b08ca352c183afa1

--- include/sdbusplus/async/task.hpp
+++ include/sdbusplus/async/task.hpp
@@ -1,12 +1,12 @@
 #pragma once
 
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wunused-parameter"
-#include <sdbusplus/async/stdexec/task.hpp>
-#pragma GCC diagnostic pop
+#include <exec/task.hpp>
 
+// Add exec::task as sdbusplus::async::task so that we can simplify reference to
+// any parts of it we use and as a portable alias that library users can
+// reference.
 namespace sdbusplus::async
 {
 template <typename T = void>
 using task = exec::task<T>;
-}
+} // namespace sdbusplus::async

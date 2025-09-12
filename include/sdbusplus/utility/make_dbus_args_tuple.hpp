7510d58a819c6cac21dbea4a40d3d3e911ab0ccb
Ed Tanous <etanous@nvidia.com>
2025-09-12 15:11:13 -0700

    Unpack in place
    The existing code uses several different tuples to do the unpack, then
    concatenates them with tuple_cat.  Simplify the code by generating a
    tuple of references to the dbus args, and unpack directly to the main
    struct, thus simplifying the flow.
    
    To accomplish this the make_dbus_args_tuple helper method is added,
    which strips the non-dbus arguments from a callback so they can be
    unpacked separately.  This is relatively complex because this code tries
    to support
    
    callback(boost::error_code, message_t, dbus_arg);
    as well as
    callback(boost::error_code, dbus_arg);
    
    Dynamically dependent on which overload is provided.  There's likely
    more cleanup that can happen there to separate these two, but this
    should at least help.
    
    Tested: OpenBMC launches and runs.  Redfish service validator (which
    should hit the majority of these overloads) passes.
    
    Change-Id: I6bb7ee960459a505eb8633b1cdb30cd2b3037466
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

---
+++ include/sdbusplus/utility/make_dbus_args_tuple.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <sdbusplus/message.hpp>
+
+#include <cstddef>
+#include <cstdint>
+#include <tuple>
+#include <utility>
+
+namespace sdbusplus
+{
+namespace utility
+{
+namespace details
+{
+template <std::size_t FirstArgIndex, typename Tuple, std::size_t... Is>
+auto make_sub_tuple_impl(Tuple& t, std::index_sequence<Is...>)
+{
+    return std::tie(std::get<FirstArgIndex + Is>(t)...);
+}
+
+template <typename TupleType>
+constexpr size_t args_to_skip()
+{
+    constexpr std::size_t input_size = std::tuple_size_v<TupleType>;
+    if constexpr (input_size > 1)
+    {
+        if (std::is_same_v<std::tuple_element_t<1, TupleType>,
+                           sdbusplus::message_t>)
+        {
+            return 2;
+        }
+    }
+    return 1;
+}
+
+} // namespace details
+
+/*
+ * Given a tuple of function args to a method callback, return a tuple
+ * with references to only the dbus message arguments.
+ * */
+template <typename... Args>
+constexpr auto make_dbus_args_tuple(std::tuple<Args...>& t)
+{
+    using tuple_type = std::remove_reference_t<decltype(t)>;
+    constexpr std::size_t input_size = std::tuple_size_v<tuple_type>;
+    constexpr size_t skip_args = details::args_to_skip<tuple_type>();
+    constexpr std::size_t new_size = input_size - skip_args;
+    return details::make_sub_tuple_impl<skip_args>(
+        t, std::make_index_sequence<new_size>{});
+}
+} // namespace utility
+} // namespace sdbusplus

8f9a3860d3e50cbf17459e54876216632a2d7a2f
Alexander Hansen <alexander.hansen@9elements.com>
2025-11-19 17:52:20 +0100

    support associations in generated headers
    There are use-cases where we want to use a given association which may
    be on a given DBus interface. e.g. [1].
    
    Rather than hardcoding the association and it's reverse form, provide a
    symbol to get this from the generated common header.
    
    Couple of advantages:
    
    - Causes build failure in applications depending on an association on a
      given interface. If the association is removed from the PDI yaml, the
      application may fail at compile time rather than at runtime, improving
      the failure mode.
    
    - It is clear to the reader/reviewer that the association may exist on
      the interface. There is less need to check documentation if this style
      is used.
    
    The feature requires changes in PDI [2] as there are some typos
    currently in how the associations are documented.
    
    Possible follow-up works:
    
    Provide symbols for the `required_endpoint_interfaces`. Did not do this
    here but provided a struct for future expansion of the information we
    may want to expose about an association on a given interface.
    
    Tested: New Unit Test Pass
    
    References:
    [1] https://github.com/openbmc/phosphor-bmc-code-mgmt/blob/3638c243ec185766e2db2498e9593a0c1649fde7/common/src/software.cpp#L90
    [2] https://gerrit.openbmc.org/c/openbmc/phosphor-dbus-interfaces/+/85443
    
    Change-Id: Icb0d5e42c176a81841b9c7986e7501c7b9d49593
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

---
+++ tools/sdbusplus/association.py
@@ -0,0 +1,26 @@
+from .namedelement import NamedElement
+from .renderer import Renderer
+
+
+class ReverseAssociation(NamedElement, Renderer):
+
+    def __init__(self, **kwargs):
+
+        reverse_name = kwargs.pop("reverse_name", "")
+        dict1 = {"name": reverse_name}
+
+        super(ReverseAssociation, self).__init__(**dict1)
+
+
+class Association(NamedElement, Renderer):
+
+    def __init__(self, **kwargs):
+
+        self.description = kwargs.pop("description", None)
+        self.required_endpoint_interfaces = kwargs.pop(
+            "required_endpoint_interfaces", None
+        )
+
+        self.reverse_name = ReverseAssociation(**kwargs)
+
+        super(Association, self).__init__(**kwargs)

e3f2296e2f5906cd1614fe4f5ca45dfc8bd38dba
Patrick Williams <patrick@stwcx.xyz>
2026-01-08 11:44:57 -0500

    test: clang-tidy: clean up bugprone-unchecked-optional-access
    Remove usage of std::optional and use std::unique_ptr instead
    to avoid clang spurious "bugprone-unchecked-optional-access"
    warnings.  In all cases we initialized the optional (or unique_ptr)
    but clang-tidy is overzealous.
    
    Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
    Change-Id: I728ac665a765bff20338c4a82a12bb1accf9698d
    Signed-off-by: Akash Arunkumar <mirrorghost007@gmail.com>

--- test/async/context.cpp
+++ test/async/context.cpp
@@ -4,39 +4,33 @@
 
 struct Context : public testing::Test
 {
+    Context() : ctx(std::make_unique<sdbusplus::async::context>()) {}
     ~Context() noexcept override = default;
+    auto& get_ctx()
+    {
+        return *ctx;
+    }
 
     void TearDown() override
     {
         // Destructing the context can throw, so we have to do it in
         // the TearDown in order to make our destructor noexcept.
-        context.reset();
-    }
-
-    // Local accessor that makes clang-tidy's flow analysis happy.
-    // It provides a local guard that the optional is engaged.
-    auto ctx() -> sdbusplus::async::context&
-    {
-        if (!context.has_value())
-        {
-            throw std::logic_error("Context is not initialized");
-        }
-        return context.value();
+        ctx.reset();
     }
 
     void spawnStop()
     {
-        ctx().spawn(stdexec::just() |
-                    stdexec::then([this]() { ctx().request_stop(); }));
+        get_ctx().spawn(stdexec::just() |
+                        stdexec::then([this]() { get_ctx().request_stop(); }));
     }
 
     void runToStop()
     {
         spawnStop();
-        ctx().run();
+        get_ctx().run();
     }
 
-    std::optional<sdbusplus::async::context> context{std::in_place};
+    std::unique_ptr<sdbusplus::async::context> ctx;
 };
 
 TEST_F(Context, RunSimple)
@@ -46,7 +40,7 @@ TEST_F(Context, RunSimple)
 
 TEST_F(Context, SpawnedTask)
 {
-    ctx().spawn(stdexec::just());
+    get_ctx().spawn(stdexec::just());
     runToStop();
 }
 
@@ -55,7 +49,7 @@ TEST_F(Context, ReentrantRun)
     runToStop();
     for (int i = 0; i < 100; ++i)
     {
-        ctx().run();
+        get_ctx().run();
     }
 }
 
@@ -67,8 +61,8 @@ TEST_F(Context, SpawnDelayedTask)
     auto start = std::chrono::steady_clock::now();
 
     bool ran = false;
-    ctx().spawn(sdbusplus::async::sleep_for(ctx(), timeout) |
-                stdexec::then([&ran]() { ran = true; }));
+    get_ctx().spawn(sdbusplus::async::sleep_for(get_ctx(), timeout) |
+                    stdexec::then([&ran]() { ran = true; }));
 
     runToStop();
 
@@ -98,8 +92,9 @@ TEST_F(Context, SpawnRecursiveTask)
     static constexpr size_t count = 100;
     size_t executed = 0;
 
-    ctx().spawn(_::one(count, executed) |
-                stdexec::then([=](auto result) { EXPECT_EQ(result, count); }));
+    get_ctx().spawn(_::one(count, executed) | stdexec::then([=](auto result) {
+                        EXPECT_EQ(result, count);
+                    }));
 
     runToStop();
 
@@ -114,13 +109,15 @@ TEST_F(Context, DestructMatcherWithPendingAwait)
     auto m = std::make_optional<sdbusplus::async::match>(
         ctx(), sdbusplus::match_rules::interfacesAdded(
                    "/this/is/a/bogus/path/for/SpawnMatcher"));
+        get_ctx(), sdbusplus::bus::match::rules::interfacesAdded(
+                       "/this/is/a/bogus/path/for/SpawnMatcher"));
 
     // Await the match completion (which will never happen).
-    ctx().spawn(m->next() | stdexec::then([&ran](...) { ran = true; }));
+    get_ctx().spawn(m->next() | stdexec::then([&ran](...) { ran = true; }));
 
     // Destruct the match.
-    ctx().spawn(sdbusplus::async::sleep_for(ctx(), 1ms) |
-                stdexec::then([&m](...) { m.reset(); }));
+    get_ctx().spawn(sdbusplus::async::sleep_for(get_ctx(), 1ms) |
+                    stdexec::then([&m](...) { m.reset(); }));
 
     runToStop();
     EXPECT_FALSE(ran);
@@ -133,6 +130,8 @@ TEST_F(Context, DestructMatcherWithPendingAwaitAsTask)
     auto m = std::make_optional<sdbusplus::async::match>(
         ctx(), sdbusplus::match_rules::interfacesAdded(
                    "/this/is/a/bogus/path/for/SpawnMatcher"));
+        get_ctx(), sdbusplus::bus::match::rules::interfacesAdded(
+                       "/this/is/a/bogus/path/for/SpawnMatcher"));
 
     struct _
     {
@@ -146,9 +145,9 @@ TEST_F(Context, DestructMatcherWithPendingAwaitAsTask)
     };
 
     bool ran = false;
-    ctx().spawn(_::fn(m->next(), ran));
-    ctx().spawn(sdbusplus::async::sleep_for(ctx(), 1ms) |
-                stdexec::then([&]() { m.reset(); }));
+    get_ctx().spawn(_::fn(m->next(), ran));
+    get_ctx().spawn(sdbusplus::async::sleep_for(get_ctx(), 1ms) |
+                    stdexec::then([&]() { m.reset(); }));
 
     runToStop();
     EXPECT_FALSE(ran);

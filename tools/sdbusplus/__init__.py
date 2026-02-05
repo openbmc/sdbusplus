7eb5130c492b12ab108d96af4781dc97dfdf7458
Alexander Hansen <alexander.hansen@9elements.com>
2026-02-05 09:35:28 +0100

    greppable interface names
    Implements a large struct to be able to access interface names in a way
    that makes them greppable.
    
    Currently we can go like this:
    
    sdbusplus::common::xyz::openbmc_project::inventory::Item;
    sdbusplus::common::xyz::openbmc_project::inventory::Item::interface;
    
    and declare a 'using' directive to shorten that.
    
    With this patch we can include the full tree and access interface names
    like below.
    
    ```
     #include ".../full_tree.hpp"
    
    lg2::debug("{INTF}", "INTF",
    xyz.openbmc_project.Inventory.Item.interface);
    ```
    
    This fulfills the requirement of being able to grep for interface names,
    provided clang-format does not decide to split the member access.
    
    The requirement to grep for interface names [1] was given by ToF
    members as prerequisite to proceed with usage of the generated code.
    
    Tradeoff: clang-format may split interface names
    
    This can be avoided either by configuration or assigning to a local
    variable. Alternatively the grep command can also be adjusted to find
    such split cases.
    
    Alternatives considered:
    
    - Defining a separate full tree without inheriting from the interfaces
      common type.
      This would avoid including all the interface common headers, limiting
      us to the interface name only.
    
    - Conditional compilation of the struct members depending on which of
      the common headers were included.
      This would force include order dependency or possible ODR violation
      for the struct type.
    
    - Defining a namespace for the full tree.
      This would increase the length of the identifier to write and force
      `using namespace` or greater likelihood of the identifier being split
      by clang-format.
    
    References:
    
    [1] https://gerrit.openbmc.org/c/openbmc/docs/+/86505/comments/d00a8148_e38bbdf8
    
    Change-Id: I16c9b2515240fdf3cc51f2a715c85290bd029afe
    Signed-off-by: Alexander Hansen <alexander.hansen@9elements.com>

--- tools/sdbusplus/__init__.py
+++ tools/sdbusplus/__init__.py
@@ -1,5 +1,6 @@
 from sdbusplus.error import Error
 from sdbusplus.event import Event
+from sdbusplus.fulltree import FullTree
 from sdbusplus.interface import Interface
 
-__all__ = ["Error", "Event", "Interface"]
+__all__ = ["Error", "Event", "FullTree", "Interface"]

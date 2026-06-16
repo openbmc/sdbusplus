openbmc/sdbusplus

commit 8017aca7d579b1992cd88e6fe44bcf5ae960001e

exception: fix use-after-free
ASAN testing revealed a likely use-after-free in the SdBusError class.
The class previously tried to take ownership of the sd_bus_error pointer
but the problem is that we cannot guarantee how far up the call-chain
the ownership of the pointer belongs.  With ASAN on a new test case, it
was observed that on the async-client path, the sd_bus_error was still
owned by the sd_bus_message object and was automatically cleaned when
the message was freed.  Later, an owner of the SdBusError attempting to
observe state like the `name` field would trigger a use-after-free.

Switch to use `sd_bus_error_copy` rather than simply moving pointers.
This ensures that the SdBusError has an explicit copy of all necessary
fields.

Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: I501bb35f7e654f3f516e57ce853c657afdd56512

src/exception.cpp
@@ -36,8 +36,7 @@ SdBusError::SdBusError(int error_in, const char* prefix,
 {}
 
 SdBusError::SdBusError(int error_in, std::string&& prefix,
-                       SdBusInterface* intf_in) :
-    error(SD_BUS_ERROR_NULL), intf(intf_in)
+                       SdBusInterface* intf_in) : intf(intf_in)
 {
     // We can't check the output of intf->sd_bus_error_set_errno() because
     // it returns the input errorcode. We don't want to try and guess
@@ -53,11 +52,9 @@ SdBusError::SdBusError(int error_in, std::string&& prefix,
 }
 
 SdBusError::SdBusError(sd_bus_error* error_in, const char* prefix,
-                       SdBusInterface* intf_in) :
-    error(*error_in), intf(intf_in)
+                       SdBusInterface* intf_in) : intf(intf_in)
 {
-    // We own the error so remove the caller's reference
-    *error_in = SD_BUS_ERROR_NULL;
+    intf->sd_bus_error_copy(&error, error_in);
 
     populateMessage(std::string(prefix));
 }

test/exception/sdbus_error.cpp
@@ -146,11 +146,8 @@ TEST(SdBusError, BasicError)
     const int errorVal = sd_bus_error_get_errno(&error);
     SdBusError err(&error, prefix.c_str());
 
-    // We expect a move not copy
-    EXPECT_EQ(nameBeforeMove, err.name());
+    EXPECT_STREQ(nameBeforeMove, err.name());
 
-    // The SdBusError should have moved our error so it should be freeable
-    EXPECT_FALSE(sd_bus_error_is_set(&error));
     sd_bus_error_free(&error);
     sd_bus_error_free(&error);
 

include/sdbusplus/exception.hpp
@@ -122,7 +122,7 @@ class SdBusError final : public internal_exception
     const sd_bus_error* get_error() const noexcept;
 
   private:
-    sd_bus_error error;
+    sd_bus_error error = SD_BUS_ERROR_NULL;
     std::string full_message;
     SdBusInterface* intf;
 

include/sdbusplus/sdbus.hpp
@@ -61,6 +61,7 @@ class SdBusInterface
     virtual int sd_bus_error_set_errno(sd_bus_error* e, int error) = 0;
     virtual int sd_bus_error_is_set(const sd_bus_error* e) = 0;
     virtual void sd_bus_error_free(sd_bus_error* e) = 0;
+    virtual int sd_bus_error_copy(sd_bus_error* dst, const sd_bus_error* e) = 0;
 
     virtual sd_event* sd_bus_get_event(sd_bus* bus) = 0;
     virtual int sd_bus_get_fd(sd_bus* bus) = 0;
@@ -301,6 +302,11 @@ class SdBusImpl : public SdBusInterface
     void sd_bus_error_free(sd_bus_error* e) override
     {
         return ::sd_bus_error_free(e);
+    }
+
+    int sd_bus_error_copy(sd_bus_error* dst, const sd_bus_error* e) override
+    {
+        return ::sd_bus_error_copy(dst, e);
     }
 
     sd_event* sd_bus_get_event(sd_bus* bus) override

include/sdbusplus/test/sdbus_mock.hpp
@@ -56,6 +56,8 @@ class SdBusMock : public SdBusInterface
     MOCK_METHOD(int, sd_bus_error_set_errno, (sd_bus_error*, int), (override));
     MOCK_METHOD(int, sd_bus_error_is_set, (const sd_bus_error*), (override));
     MOCK_METHOD(void, sd_bus_error_free, (sd_bus_error*), (override));
+    MOCK_METHOD(int, sd_bus_error_copy, (sd_bus_error*, const sd_bus_error*),
+                (override));
 
     MOCK_METHOD(sd_event*, sd_bus_get_event, (sd_bus*), (override));
     MOCK_METHOD(int, sd_bus_get_fd, (sd_bus*), (override));

commit 5fd698531e4a3237f4bad355adbfdb345e5d6591

exception: handle sd_bus_error ownership properly
An earlier commit attempted to fix a use-after-free but in doing so
introduced a memory leak in many error handling paths.  The SdBusError
constructor has a comment indicating that the SdBusError is supposed to
take ownership of the error; the previous commit undid that ownership
transfer and attempted to use `sd_bus_error_copy` to create a new copy.
This is not the documented behavior of SdBusError and is now reverted.

The earlier commit was trying to fix a use-after-free case which
happened to only be in a single code path used in async clients.  When
`sd_bus_message_get_error` (or sdbusplus equivalent) is used, the
message maintains ownership of the error and simply adjusting pointers
does not change that.  In the code path we need to create a new / copy
of the error so that we can transfer ownership properly into the
SdBusError class.

Fixes: 8017aca7d579 ("exception: fix use-after-free")
Signed-off-by: Patrick Williams <patrick@stwcx.xyz>
Change-Id: Ibf49aae7a81a929f992b82fc0e8ef86b764b319c

include/sdbusplus/async/callback.hpp
@@ -85,7 +85,8 @@ struct callback_operation
             // Check the message response for error.
             if (msg.is_method_error())
             {
-                auto err = *msg.get_error();
+                sd_bus_error err = SD_BUS_ERROR_NULL;
+                sd_bus_error_copy(&err, msg.get_error());
                 throw exception::SdBusError(&err, "method");
             }
 

src/exception.cpp
@@ -52,9 +52,11 @@ SdBusError::SdBusError(int error_in, std::string&& prefix,
 }
 
 SdBusError::SdBusError(sd_bus_error* error_in, const char* prefix,
-                       SdBusInterface* intf_in) : intf(intf_in)
+                       SdBusInterface* intf_in) :
+    error(*error_in), intf(intf_in)
 {
-    intf->sd_bus_error_copy(&error, error_in);
+    // We own the error so remove the caller's reference
+    *error_in = SD_BUS_ERROR_NULL;
 
     populateMessage(std::string(prefix));
 }

test/exception/sdbus_error.cpp
@@ -146,8 +146,11 @@ TEST(SdBusError, BasicError)
     const int errorVal = sd_bus_error_get_errno(&error);
     SdBusError err(&error, prefix.c_str());
 
-    EXPECT_STREQ(nameBeforeMove, err.name());
+    // We expect a move not copy
+    EXPECT_EQ(nameBeforeMove, err.name());
 
+    // The SdBusError should have moved our error so it should be freeable
+    EXPECT_FALSE(sd_bus_error_is_set(&error));
     sd_bus_error_free(&error);
     sd_bus_error_free(&error);
 